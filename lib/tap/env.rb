f348fb1ed158378eaf1d5d85adbd10405d5d5499
Simon Chiang <simon.a.chiang@gmail.com>
2008-05-11 11:03:09 -0600

    Additional work on Env.  Env now is functional, but not fully documented.  Some setups and gem methods are still untested.

--- lib/tap/env.rb
+++ lib/tap/env.rb
@@ -4,14 +4,14 @@ autoload(:PP, "pp")
 module Tap
   class Env
     @@instance = nil
-    
+
     class << self
-      
+
       # Returns the active instance of Env.
       def instance
         @@instance
       end
-      
+
       # Templates the input filepath using ERB then loads it as YAML.  
       # Returns an empty hash if the file doesn't exist, or loads to
       # nil or false (as for an empty file).  Raises an error if the
@@ -51,61 +51,65 @@ module Tap
         spec.full_gem_path
       end
     end
-    
+
     include Support::Configurable
     
     # Specify gems to add to the environment.  Versions may be specified.
     config :gems, []
     
     # Specify configuration files to load into env.
-    config :config_paths, [] # :nodoc:
-  
+    config :config_paths, [] 
+
     # Designate load paths.  If use_dependencies == true, then
     # load_paths will be used for automatic loading of modules
     # through the active_support Dependencies module.
     config :load_paths, ["lib"]
-    
+
     # Specifies automatic loading of modules through Dependencies. 
     config :use_dependencies, true
-    
+
     # Designate paths for discovering and executing commands. 
     config :command_paths, ["cmd"]
 
     # Designate paths for discovering generators.  
     config :generator_paths, ["lib/generators"]
-    
+
     # An array of config keys that are resolved using root
     # when set through configure.
     PATH_CONFIGS = [:config_paths, :load_paths, :command_paths, :generator_paths]
-    
+
+    # An array of config keys that can be set in the recursive context.
+    RECURSIVE_CONFIGS = [:load_paths, :command_paths, :generator_paths]
+
     # An array of config keys that are used by configure
     # to reconfigure the root input.
     ROOT_CONFIGS = [:root, :directories, :absolute_paths]
-    
-    #DEFAULT_CONFIG_FILE = "tap.yml"
-    
+
+    # The default config file path
+    DEFAULT_CONFIG_FILE = "tap.yml"
+
     # Gets or sets the logger for self
     attr_accessor :logger
-    
-    def initialize(config={}, logger=nil)
-      self.config = config
+
+    def initialize(logger=nil)
+      self.config = {}
       @logger = logger
-      #configure(config)
+      @recursive = false
     end
-    
+
     # Logs the action and message at the input level (default INFO).
     # Logging is suppressed if no logger is set.
     def log(action, msg="", level=Logger::INFO)
-     logger.add(level, msg, action.to_s) if logger
+      logger.add(level, msg, action.to_s) if logger
     end
-    
+
     # Returns a list of arrays that receive load_paths on activate,
     # by default [$LOAD_PATH]. If use_dependencies == true, then
     # Dependencies.load_paths will also be included.
     def load_path_targets
       use_dependencies ? [$LOAD_PATH, Dependencies.load_paths] : [$LOAD_PATH]
     end
-    
+
     # Activates self by unshifting load_paths for self to the load_path_targets.
     # Once active, self can be referenced from Env.instance and the current
     # configurations are frozen.  Env.instance is deactivated, if set, before
@@ -113,116 +117,138 @@ module Tap
     # is already active.
     def activate
       return false if active?
-      
+
       unless Env.instance == nil
         Env.instance.deactivate
       end
-      
+
       @@instance = self
       config.each_pair do |key, value|
         case value
         when Array then config[key] = value.dup.freeze
         end
       end
-      
+
       load_path_targets.each do |target|
         load_paths.reverse_each do |path|
           target.unshift(path)
         end
-        
+
         target.uniq!
       end
-      
+
       true
     end
-    
+
     # Deactivates self by deleting load_paths for self from the load_path_targets.
     # Env.instance will no longer reference self and the configurations are 
     # unfrozen (using duplication as needed, but it amounts to the same thing).
     # Returns true if deactivate succeeded, or false if self is not active.
     def deactivate
       return false unless active?
-      
+
       @@instance = nil
       config.each_pair do |key, value|
         case value
         when Array then config[key] = value.dup
         end
       end
-      
+
       load_path_targets.each do |target|
         load_paths.each do |path|
           target.delete(path)
         end
       end
-      
+
       true
     end
-    
+
     # Returns true if Env.instance == self
     def active?
       Env.instance == self
     end
-    
+
     #--
+    # Processes and sets the input configurations. The configuration process
+    # consists of the following steps:
+    #
+    # * partition config into env, root, and other configs
+    # * reconfigure root with the root configs
+    # * resolve PATH_CONFIGS using root
+    # * recursively load/configure :config_paths and :gems
+    # * set configurations
+    # * yield unhandled_configs to the block (if given)
+    #
     # Note: always yields to the block, even if non_env_configs is empty
-    def configure(config, root=Tap::Root.new, &block) # :yields: non_env_configs
-      
+    def configure(config, root=Tap::Root.new, &block) # :yields: unhandled_configs
+
       # partiton config into its parts
       env_configs = class_configurations.default(true)
       root_configs = {}
       other_configs = {}
-      
+
       config.each_pair do |key, value|
         key = class_configurations.normalize_key(key)
-        
+
         partition = case 
         when class_configurations.has_config?(key) then env_configs
         when ROOT_CONFIGS.include?(key) then root_configs
         else other_configs
         end
-        
+
         partition[key] = value
       end
-      
+
       # assign root configs, for resolution of paths later
       unless root_configs.empty?
         root.send(:assign_paths,
-          root_configs[:root] || root.root, 
-          root_configs[:directories] || root.directories, 
-          root_configs[:absolute_paths] || root.absolute_paths
+        root_configs[:root] || root.root, 
+        root_configs[:directories] || root.directories, 
+        root_configs[:absolute_paths] || root.absolute_paths
         )
       end
-      
-      # resolve path_configs
+
+      # resolve path_configs, unshifting to existing
       PATH_CONFIGS.each do |key|
         paths = env_configs[key]
-        
+
         # arrayify
         paths = case paths
         when Array then paths
         when nil then []
         else [paths]
         end
-        
-        env_configs[key] = paths.collect {|path| root[path] }
-      end
-    
-      # load config_paths
-      env_configs.delete(:config_paths).each do |path|
-        load_config(path)
+
+        env_configs[key] = paths.collect {|path| root[path] } 
       end
 
-      # load gems
-      env_configs.delete(:gems).each do |gem_name|
-        load_gem(gem_name)
+      recursive_context do
+        # load config_paths  
+        env_configs.delete(:config_paths).each do |path|
+          if load_config(path)
+            RECURSIVE_CONFIGS.each {|key| env_configs[key].concat(get_config(key)) }
+          end
+        end
+
+        # load gems
+        env_configs.delete(:gems).each do |gem_name|
+          if load_gem(gem_name)
+            RECURSIVE_CONFIGS.each {|key| env_configs[key].concat(get_config(key)) }
+          end
+        end
       end
- 
+
+      # remove duplicates once recursive loading is done
+      unless in_recursive_context?
+        RECURSIVE_CONFIGS.each {|key| env_configs[key].uniq! }
+      end
+
       # set remaining env configs
       env_configs.each_pair do |key, value|
+        next if in_recursive_context? && !RECURSIVE_CONFIGS.include?(key)
         set_config(key, value)
       end
-      
+
       # handle unknown configs 
       case
       when block_given?
@@ -230,148 +256,166 @@ module Tap
       when !other_configs.empty?
         log(:warn, "ignoring non-env configs: #{other_configs.keys.join(',')}", Logger::DEBUG)
       end
-      
+
       true
     end
-    
-    def load_config(path, root=Tap::Root.new, &block) # :yields: non_env_configs
+
+    def load_config(path, root=Tap::Root.new, &block) # :yields: unhandled_configs
       path = File.join(path, DEFAULT_CONFIG_FILE) if File.directory?(path)
       path = File.expand_path(path)
 
       # prevent infinite looping
       return false if config_paths.include?(path)
-    
+
       # load config
       log(:load_config, path, Logger::DEBUG)
       config_paths << path
-    
+
       config = Env.read_config(path)
       unless config.has_key?(:root) || config.has_key?('root')
         config[:root] = File.dirname(path) 
       end
-    
+
       configure(config, root, &block)
     end
-    
+
     # Loads the config for the specified gem.  A gem version can be 
     # specified in the name, see full_gem_path.
     def load_gem(gem_name)
       # prevent looping
       full_gem_path = Env.full_gem_path(gem_name)
       return false if gems.include?(full_gem_path)
-      
+
       gems << full_gem_path
       load_config(full_gem_path)
     end
+
+    # Simply yields to the manditory block.  Within the block
+    # in_recursive_context? returns true. (useful for changing
+    # the behavior of methods like configure, which must act
+    # differently when recursively loading config files)
+    def recursive_context
+      if in_recursive_context?
+        yield
+      else
+        @recursive = true
+        yield
+        @recursive = false
+      end
+    end
+
+    # True if within a recursive_context block, false otherwise.
+    def in_recursive_context?
+      @recursive
+    end
+
+    #--
+    # Searches for and returns all .rb files under each of the command_paths
+    # as well as the default tap commands.  Commands with conflicting names
+    # raise an error; however, user commands are allowed to override the
+    # default tap commands and will NOT raise an error.
+    def commands(command_pattern="**/*.rb")
+      commands = {}
+      command_paths.reverse_each do |command_path|
+        Dir.glob( File.join(command_path, command_pattern) ).each do |path|
+          cmd = Tap::Root.relative_filepath(command_path, path).chomp(".rb")
+
+          if commands.include?(cmd)
+            log(:warn, "command name confict: #{cmd} (overriding '#{commands[cmd]}' with '#{path}')")
+          end
+
+          commands[cmd] = path
+        end
+      end
+      commands
+    end
+
+    #
+    # under construction...
+    #
+    
+    #--
+    # Returns the path to all DEFAULT_CONFIG_FILEs for installed gems.
+    # If latest==true, then only the config files for the latest gem
+    # specs will be returned (ie for the most current version of a
+    # gem).
+    def gem_config_files(latest=true)
+      if latest
+        Gem.source_index.latest_specs.collect do |spec| 
+          config_file = File.join(spec.full_gem_path, DEFAULT_CONFIG_FILE)
+          File.exists?(config_file) ? config_file : nil
+        end.compact
+      else
+        Gem.path.collect do |dir|
+          Dir.glob( File.join(dir, "gems/*", DEFAULT_CONFIG_FILE) )
+        end.flatten.uniq
+      end
+    end
+
+    # Loads the config files discovered by gem_config_files(true).
+    def discover_gems
+      gem_config_files.collect do |config_file|
+        load_config(config_file)
+      end
+    end
+
+    def debug_setup
+      $DEBUG = true
+      logger.level = Logger::DEBUG if logger
+    end
+
+    def rails_setup(app=Tap::App.instance)
+      Object.const_set('RAILS_ROOT', app.root)
+      Object.const_set('RAILS_DEFAULT_LOGGER', app.logger)
+      Dependencies.log_activity = app.debug?
+    end
+
+    #--
+    # TODO -- get this to only run once
+    def rake_setup(argv=ARGV, app=Tap::App.instance)
+      Tap::Support.autoload(:Rake, 'tap/support/rake')
+
+      # setup
+      app.extend Tap::Support::Rake
+      rake = Rake.application.extend Tap::Support::Rake::Application
+      rake.on_standard_exception do |error|
+        if error.message =~ /^No Rakefile found/
+          log(:warn, error.message, Logger::DEBUG)
+        else raise error
+        end
+      end
+
+      options = rake.options
+
+      # merge options down from app
+      app.options.marshal_dump.each_pair do |key, value|
+        options.send("#{key}=", value)
+      end
+      options.silent = true
+
+      # run as if from command line using argv
+      current_argv = ARGV.dup
+      begin
+        ARGV.concat(argv)
+
+        # now follow the same protocol as 
+        # in run, handling options
+        rake.init
+        rake.load_rakefile
+      ensure
+        ARGV.clear
+        ARGV.concat(current_argv)
+      end
+
+      rake
+    end
     
     protected
-    
-    def set_config(key, value)
+
+    def set_config(key, value, process=true)
       raise "config modification is disabled when active" if active?
       super
     end
-    
-    # def debug_setup
-    #    $DEBUG = true
-    #    logger.level = Logger::DEBUG
-    #  end
-    #  
-    #  def rails_setup(app=Tap::App.instance)
-    #    Object.const_set('RAILS_ROOT', app.root)
-    #    Object.const_set('RAILS_DEFAULT_LOGGER', app.logger)
-    #    Dependencies.log_activity = app.debug?
-    #  end
-    #  
-    #  #--
-    #  # TODO -- get this to only run once
-    #  def rake_setup(argv=ARGV, app=Tap::App.instance)
-    #    Tap::Support.autoload(:Rake, 'tap/support/rake')
-    # 
-    #    # setup
-    #    app.extend Tap::Support::Rake
-    #    rake = Rake.application.extend Tap::Support::Rake::Application
-    #    rake.on_standard_exception do |error|
-    #      if error.message =~ /^No Rakefile found/
-    #        log(:warn, error.message, Logger::DEBUG)
-    #      else raise error
-    #      end
-    #    end
-    #    
-    #    options = rake.options
-    #  
-    #    # merge options down from app
-    #    app.options.marshal_dump.each_pair do |key, value|
-    #      options.send("#{key}=", value)
-    #    end
-    #    options.silent = true
-    #  
-    #    # run as if from command line using argv
-    #    current_argv = ARGV.dup
-    #    begin
-    #      ARGV.concat(argv)
-    #      
-    #      # now follow the same protocol as 
-    #      # in run, handling options
-    #      rake.init
-    #      rake.load_rakefile
-    #    ensure
-    #      ARGV.clear
-    #      ARGV.concat(current_argv)
-    #    end
-    #    
-    #    rake
-    #  end
-    # 
-    #  
-    #  # Returns the path to all DEFAULT_CONFIG_FILEs for installed gems.
-    #  # If latest==true, then only the config files for the latest gem
-    #  # specs will be returned (ie for the most current version of a
-    #  # gem).
-    #  def gem_config_files(latest=true)
-    #    if latest
-    #      Gem.source_index.latest_specs.collect do |spec| 
-    #        config_file = File.join(spec.full_gem_path, DEFAULT_CONFIG_FILE)
-    #        File.exists?(config_file) ? config_file : nil
-    #      end.compact
-    #    else
-    #      Gem.path.collect do |dir|
-    #        Dir.glob( File.join(dir, "gems/*", DEFAULT_CONFIG_FILE) )
-    #      end.flatten.uniq
-    #    end
-    #  end
-    #  
-    #  # Loads the config files discovered by gem_config_files(true).
-    #  def discover_gems
-    #    gem_config_files.collect do |config_file|
-    #      load_config(config_file)
-    #    end
-    #  end
-    #  
-    #  # Searches for and returns all .rb files under each of the command_paths
-    #  # as well as the default tap commands.  Commands with conflicting names
-    #  # raise an error; however, user commands are allowed to override the
-    #  # default tap commands and will NOT raise an error.
-    #  def commands
-    #    commands = {}
-    #    config['command_paths'].each do |path|
-    #      pattern = File.join(path, "**/*.rb")
-    #      
-    #      Dir.glob(pattern).each do |file|
-    #        cmd = Tap::App.relative_filepath(path, file).chomp(".rb")
-    #        raise "command name confict: #{cmd}" if commands.include?(cmd)
-    #        commands[cmd] = file
-    #      end
-    #    end
-    # 
-    #    # allow all other scripts to override default scripts
-    #    # (hence do this second)
-    #    tap_command_dir = File.expand_path(File.join( File.dirname(__FILE__), "cmd"))
-    #    Dir.glob( tap_command_dir + "/**/*.rb" ).each do |file|
-    #      cmd = Tap::App.relative_filepath(tap_command_dir, file).chomp(".rb")
-    #      commands[cmd] = file unless commands.include?(cmd)
-    #    end
-    # 
-    #    commands
-    #  end
+
   end
 end
--- test/env_test.rb
+++ test/env_test.rb
@@ -5,7 +5,7 @@ class EnvTest < Test::Unit::TestCase
   
   acts_as_file_test
   
-  attr_accessor :e
+  attr_accessor :e, :root
   
   def setup
     super
@@ -16,6 +16,7 @@ class EnvTest < Test::Unit::TestCase
     Dependencies.load_paths.clear
     
     @e = Tap::Env.new
+    @root = Tap::Root.new
   end
   
   def teardown
@@ -39,14 +40,14 @@ class EnvTest < Test::Unit::TestCase
   #
   
   def test_read_config_templates_then_loads_config
-    config_file = output_tempfile
+    config_file = method_tempfile
     
     File.open(config_file, "wb") {|f| f << "sum: <%= 1 + 2 %>" }
     assert_equal({'sum' => 3}, Tap::Env.read_config(config_file))
   end
   
   def test_read_config_returns_empty_hash_for_non_existant_nil_and_false_files
-    config_file = output_tempfile
+    config_file = method_tempfile
     
     assert !File.exists?(config_file)
     assert_equal({}, Tap::Env.read_config(config_file))
@@ -64,7 +65,7 @@ class EnvTest < Test::Unit::TestCase
   end
   
   def test_read_config_raises_error_for_non_hash_result
-    config_file = output_tempfile
+    config_file = method_tempfile
     
     File.open(config_file, "wb") {|f| f << [].to_yaml }
     assert_raise(RuntimeError) { Tap::Env.read_config(config_file) }
@@ -315,28 +316,28 @@ class EnvTest < Test::Unit::TestCase
     e.configure({
       :load_paths => ['lib', 'alt'],
       :command_paths => ['cmd']
-    }, trs)
+    }, root)
 
-    assert_equal [trs['lib'], trs['alt']], e.load_paths
-    assert_equal [trs['cmd']], e.command_paths
+    assert_equal [root['lib'], root['alt']], e.load_paths
+    assert_equal [root['cmd']], e.command_paths
   end
   
   def test_configure_resolves_single_and_nil_values_for_PATH_CONFIGS_as_arrays
-    e.configure({:load_paths => 'lib', :command_paths => nil}, trs)
+    e.configure({:load_paths => 'lib', :command_paths => nil}, root)
 
-    assert_equal [trs['lib']], e.load_paths
+    assert_equal [root['lib']], e.load_paths
     assert_equal [], e.command_paths
   end
   
   def test_configure_merges_default_with_inputs
-    e.configure({:use_dependencies => false}, trs)
+    e.configure({:use_dependencies => false}, root)
 
     expected = {
-      :load_paths => [trs['lib']],
+      :load_paths => [root['lib']],
       :config_paths => [],
-      :command_paths => [trs['cmd']],
+      :command_paths => [root['cmd']],
       :gems => [],
-      :generator_paths => [trs['lib/generators']],
+      :generator_paths => [root['lib/generators']],
       :use_dependencies => false
     }
     
@@ -344,22 +345,20 @@ class EnvTest < Test::Unit::TestCase
   end
   
   def test_configure_reassigns_root_paths_using_configs_before_resolving_paths
-    assert_not_equal 'alt', trs['lib']
-    e.configure({:load_paths => ['lib'], :directories => {'lib' => 'alt'}}, trs)
+    assert_not_equal 'alt', root['lib']
+    e.configure({:load_paths => ['lib'], :directories => {'lib' => 'alt'}}, root)
     
-    assert_not_equal 'alt', trs['lib']
-    assert_equal [trs['alt']], e.load_paths
+    assert_not_equal 'alt', root['lib']
+    assert_equal [root['alt']], e.load_paths
   end
   
   def test_configure_normalizes_keys
-    e.configure({'load_paths' => ['lib'], 'directories' => {'lib' => 'alt'}}, trs)
-    assert_equal [trs['alt']], e.load_paths
+    e.configure({'load_paths' => ['lib'], 'directories' => {'lib' => 'alt'}}, root)
+    assert_equal [root['alt']], e.load_paths
   end
   
   def test_uses_new_root_if_none_is_provided
     e.configure(:load_paths => ['lib'])
-    
-    root = Tap::Root.new
     assert_equal [root['lib']], e.load_paths
   end
   
@@ -390,29 +389,110 @@ class EnvTest < Test::Unit::TestCase
     assert_equal [[Logger::DEBUG, "ignoring non-env configs: unused", "warn"]], e.logger
   end
   
+  def test_configure_will_only_set_RECURSIVE_CONFIGS_in_recursive_context
+    assert e.use_dependencies
+    
+    was_in_recursive_context = false
+    e.recursive_context do
+      e.configure({:use_dependencies => false}, root)
+      was_in_recursive_context = true
+    end
+    
+    assert e.use_dependencies
+    assert was_in_recursive_context
+  end
+  
+  def test_configure_loads_config_path_configs_in_recursive_context
+    config_file = method_tempfile
+    File.open(config_file, "w") do |file| 
+      file << {:load_paths => ['three'], :root => root.root}.to_yaml
+    end
+    
+    e.configure({:load_paths => ['one', 'two'], :config_paths => [config_file]}, root)
+    assert_equal [root['one'], root['two'], root['three']], e.load_paths
+  end
+  
+  def test_recursive_loading_for_multiple_recursions
+    config_file1 = method_tempfile
+    config_file2 = method_tempfile
+    config_file3 = method_tempfile
+    
+    File.open(config_file1, "w") do |file| 
+      file << {:load_paths => ['one'], :config_paths => config_file2, :root => root.root}.to_yaml
+    end
 
+    File.open(config_file2, "w") do |file| 
+      file << {:load_paths => ['two'], :config_paths => config_file3, :root => root.root}.to_yaml
+    end
+    
+    File.open(config_file3, "w") do |file| 
+      file << {:load_paths => ['three'], :root => root.root}.to_yaml
+    end
+    
+    e.configure({:load_paths => ['zero'], :config_paths => config_file1}, root)
+    assert_equal [root['zero'], root['one'], root['two'], root['three']], e.load_paths
+  end
+  
+  def test_recursive_loading_does_not_infinitely_loop
+    config_file1 = method_tempfile
+    config_file2 = method_tempfile
+    
+    File.open(config_file1, "w") do |file| 
+      file << {:load_paths => ['one'], :config_paths => config_file2, :root => root.root}.to_yaml
+    end
+  
+    File.open(config_file2, "w") do |file| 
+      file << {:load_paths => ['two'], :config_paths => config_file1, :root => root.root}.to_yaml
+    end
+    
+    e.configure({:load_paths => ['zero'], :config_paths => config_file1}, root)
+    assert_equal [root['zero'], root['one'], root['two']], e.load_paths
+  end
+  
+  def test_recursive_loading_removes_duplicates_and_preserves_order_of_first_loading
+    config_file1 = method_tempfile
+    config_file2 = method_tempfile
+    config_file3 = method_tempfile
+    
+    File.open(config_file1, "w") do |file| 
+      file << {:load_paths => ['zero', 'one'], :config_paths => config_file2, :root => root.root}.to_yaml
+    end
+
+    File.open(config_file2, "w") do |file| 
+      file << {:load_paths => ['one', 'zero', 'two'], :config_paths => config_file3, :root => root.root}.to_yaml
+    end
+    
+    File.open(config_file3, "w") do |file| 
+      file << {:load_paths => ['three', 'one', 'zero', 'two'], :root => root.root}.to_yaml
+    end
+    
+    e.configure({:load_paths => ['zero'], :config_paths => config_file1}, root)
+    assert_equal [root['zero'], root['one'], root['two'], root['three']], e.load_paths
+  end
   
   #
   # load_config test
   #
   
   def test_load_config_configures_using_configs_from_file
-    config_file = output_tempfile
-    File.open(config_file, "w") {|file| file << {:use_dependencies => false}.to_yaml }
+    config_file = method_tempfile
+    File.open(config_file, "w") do |file| 
+      file << {:use_dependencies => false}.to_yaml
+    end
     
     assert e.use_dependencies
-    e.load_config(config_file, trs)
+    e.load_config(config_file)
     assert !e.use_dependencies
   end
   
   def test_loaded_config_files_are_added_to_config_paths
-    config_file = output_tempfile
+    config_file = method_tempfile
     e.load_config(config_file)
     assert e.config_paths.include?(config_file)
   end
   
   def test_load_config_reassigns_root_to_config_file_directory_unless_root_is_a_loaded_config
-    config_file = output_tempfile
+    config_file = method_tempfile
     File.open(config_file, "w") {|file| file << {:load_paths => ['lib']}.to_yaml }
     
     e.load_config(config_file)
@@ -420,10 +500,60 @@ class EnvTest < Test::Unit::TestCase
   end
   
   def test_config_file_for_load_config_does_not_need_to_exist
-    config_file = output_tempfile
+    config_file = method_tempfile
     assert_nothing_raised { e.load_config(config_file) }
   end
   
+  def test_load_config_appends_DEFAULT_CONFIG_FILE_to_directory_configs_files
+    dir = File.dirname(__FILE__)
+    assert File.directory?(dir)
+    e.load_config(dir)
+    assert e.config_paths.include?( File.expand_path(File.join(dir, Tap::Env::DEFAULT_CONFIG_FILE)) )
+  end
+  
+  #
+  # recursive_context test
+  #
 
+  def test_in_recursive_context_is_true_if_in_recursive_context
+    assert !e.in_recursive_context?
+    
+    e.recursive_context do
+      assert e.in_recursive_context?
+      
+      e.recursive_context do
+        assert e.in_recursive_context?
+      end
+      
+      assert e.in_recursive_context?
+    end
+    
+    assert !e.in_recursive_context?
+  end
+  
+  #
+  # commands test 
+  #
+  
 
+  def test_commands_selects_commands_from_all_command_paths
+    e.command_paths << method_dir(:one)
+    e.command_paths << method_dir(:two)
+    
+    expected = {
+      'cmd_one' => method_filepath(:one, 'cmd_one.rb'),
+      'cmd_two' => method_filepath(:two, 'cmd_two.rb')
+    }
+    
+    assert_equal expected, e.commands
+  end
+  
+  def test_commands_selects_command_paths_in_reverse_order_of_command_path
+    e.command_paths << method_dir(:one)
+    e.command_paths << method_dir(:two)
+    
+    expected = {'cmd_two' => method_filepath(:one, 'cmd_two.rb')}
+    
+    assert_equal expected, e.commands
+  end
 end
