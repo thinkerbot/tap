e986be4d3ec32ccf705f27191937dfbf171eeda4
Simon Chiang <simon.a.chiang@gmail.com>
2008-07-10 08:53:59 -0600

    Updated Comment.

--- lib/tap/support/cdoc/comment.rb
+++ lib/tap/support/cdoc/comment.rb
@@ -4,13 +4,14 @@ module Tap
   module Support
     module CDoc
       
-      # Comment represents a comment parsed by CDoc.
+      # Comment represents a comment parsed by TDoc.
       class Comment
-        
+      
         class << self
-          
+        
           # Parses the input string into a comment, stopping at end_regexp
-          # or the first non-comment line.  Takes a string or a StringScanner
+          # or the first non-comment line.  Also parses the next non-comment
+          # lines as the comment target.  Takes a string or a StringScanner
           # and returns the new comment.
           #
           #   comment_string = %Q{
@@ -20,6 +21,7 @@ module Tap
           #   #   while indented lines
           #   #   are preserved individually
           #   #    
+          #   this is the target line
           #
           #   # this line is not parsed as it
           #   # is after a non-comment line
@@ -34,70 +36,173 @@ module Tap
           #   # ['  are preserved individually'],
           #   # [''],
           #   # []]
+          #   c.target_line   # => "this is the target line"
           #
-          def parse(str)
+          def parse(str) # :yields: fragment
             scanner = case str
             when StringScanner then str
             when String then StringScanner.new(str)
             else raise ArgumentError, "expected StringScanner or String"
             end
-            
-            comment = self.new
+          
+            comment = Comment.new
+            broke_on_comment = false
             while scanner.scan(/\r?\n?[ \t]*#[ \t]?(([ \t]*).*?)$/)
-              current = scanner[1]
-              whitespace = scanner[2]
+              fragment = scanner[1]
+              indent = scanner[2]
               
               # collect continuous description line
               # fragments and join into a single line
-              case
-              when block_given? && yield(current)
-                # break if the description end is reached
+              if block_given? && yield(fragment)
+                # break on comment if the description end is reached
+                broke_on_comment = true
                 break
-              when current == whitespace
-                # empty comment line
-                comment << [""]
-                comment << []
-              when whitespace.empty?
-                # continuation line
-                comment << current.rstrip
-              else 
-                # indented line
-                comment << [current.rstrip]
-                comment << []
+              else
+                categorize(fragment, indent) {|f| comment.push(f) }
               end
             end
-            
+          
+            unless broke_on_comment
+              scanner.skip(/\s+/)
+              unless scanner.peek(1) == '#'
+                comment.target_line = scanner.scan(/.+?$/) 
+                comment.target_line.strip! unless comment.target_line == nil
+              end
+            end
+          
             comment
           end
+          
+          # Scans the line checking if it is a comment.  If so, scan
+          # yields the parse fragments to the block which correspond
+          # to the type of comment input (continuation, indent, etc).
+          # Returns true if the line is a comment, false otherwise.
+          #
+          # Scan may be used to build a comment from an array of lines:
+          #
+          #   lines = [
+          #     "# comments spanning multiple",
+          #     "# lines are collected",
+          #     "#",
+          #     "#   while indented lines",
+          #     "#   are preserved individually",
+          #     "#    ",
+          #     "not a comment line",
+          #     "# skipped since the loop breaks",
+          #     "# at the first non-comment line"]
+          #
+          #   c = Comment.new
+          #   lines.each do |line|
+          #     break unless Comment.scan(line) do |fragment|
+          #       # c.unshift will also work if building in reverse
+          #       c.push(fragment)  
+          #     end
+          #   end
+          #
+          #   c.lines   
+          #   # => [
+          #   # ['comments spanning multiple', 'lines are collected'],
+          #   # [''],
+          #   # ['  while indented lines'],
+          #   # ['  are preserved individually'],
+          #   # [''],
+          #   # []]
+          #
+          def scan(line) # :yields: fragment
+            return false unless line =~ /^[ \t]*#[ \t]?(([ \t]*).*?)$/
+            categorize($1, $2) do |fragment|
+              yield(fragment)
+            end
+            true
+          end
+          
+          private
+          
+          def categorize(fragment, indent)
+             case
+             when fragment == indent
+               # empty comment line
+               yield [""]
+               yield []
+             when indent.empty?
+               # continuation line
+               yield fragment.rstrip
+             else 
+               # indented line
+               yield [fragment.rstrip]
+               yield []
+             end
+           end
         end
-        
+      
         # An array of line fragment arrays.
         attr_reader :lines
-        
-        def initialize
+      
+        # The next non-comment line after the comment ends.
+        # This is the line that would receive the comment
+        # in RDoc documentation.
+        attr_accessor :target_line
+      
+        def initialize(target_line=nil)
           @lines = [[]]
+          @target_line = target_line
         end
 
-        # Adds the fragment to the last line array.  If fragment is an
+        # Pushes the fragment onto the last line array.  If fragment is an
         # array itself, then fragment will be pushed onto lines.
         #
         #   c = Comment.new
-        #   c << "some line"
-        #   c << "fragments"
-        #   c << ["a", "whole", "new line"]
+        #   c.push "some line"
+        #   c.push "fragments"
+        #   c.push ["a", "whole", "new line"]
         #   c.lines         # => [["some line", "fragments"], ["a", "whole", "new line"]]
         #
-        def <<(fragment)
+        def push(fragment)
           case fragment
           when Array
             if lines[-1].empty? 
               lines[-1] = fragment
             else
-              lines << fragment
+              lines.push fragment
             end
           else
-             lines[-1] << fragment
+             lines[-1].push fragment
           end
+        end
+        
+        # Alias for push.
+        def <<(fragment)
+          push(fragment)
+        end
+        
+        # Unshifts the fragment to the first line array.  If fragment is an
+        # array itself, then fragment will be unshifted onto lines.
+        #
+        #   c = Comment.new
+        #   c.unshift "some line"
+        #   c.unshift "fragments"
+        #   c.unshift ["a", "whole", "new line"]
+        #   c.lines         # => [["a", "whole", "new line"], ["fragments", "some line"]]
+        #
+        def unshift(fragment)
+          case fragment
+          when Array
+            if lines[0].empty? 
+              lines[0] = fragment
+            else
+              lines.unshift fragment
+            end
+          else
+             lines[0].unshift fragment
+          end
+        end
+        
+        def prepend(comment_line)
+          Comment.scan(comment_line) {|f| unshift(f) }
+        end
+        
+        def append(comment_line)
+          Comment.scan(comment_line) {|f| push(f) }
         end
         
         # Removes leading and trailing lines that are empty ([]) 
@@ -105,15 +210,16 @@ module Tap
         def trim
           lines.shift while !lines.empty? && (lines[0].empty? || lines[0].join.strip.empty?)
           lines.pop while !lines.empty? && (lines[-1].empty? || lines[-1].join.strip.empty?)
-          
-          lines << [] if lines.empty?
+        
+          lines.push [] if lines.empty?
           self
         end
         
+        # True if there are no fragments in self.
         def empty?
           !lines.find {|array| !array.empty?}
         end
-        
+      
         # Returns lines as a string where line fragments are joined by
         # fragment_sep and lines are joined by line_sep.  If cols is
         # specified, each line will be wrapped with the specified
@@ -123,7 +229,7 @@ module Tap
           resolved_lines = lines.collect do |line|
             line_str = line.join(fragment_sep)
             line_str = line_str.gsub(/\t/, " " * tabsize) unless tabsize == nil
-            
+          
             if cols == nil || line_str.strip.empty? 
               line_str
             else
@@ -132,9 +238,10 @@ module Tap
               line_str.gsub(/(.{1,#{cols}})( +|$\r?\n?)|(.{1,#{cols}})/, "\\1\\3\n").split(/\s*\n/)
             end
           end.flatten
-          
+        
           line_sep ? resolved_lines.join(line_sep) : resolved_lines
         end
+
       end
     end
   end
--- test/support/cdoc/comment_test.rb
+++ test/support/cdoc/comment_test.rb
@@ -22,6 +22,7 @@ class CommentTest < Test::Unit::TestCase
 #   while indented lines
 #   are preserved individually
 #    
+this is the target line
 
 # this line is not parsed as it
 # is after a non-comment line
@@ -35,6 +36,8 @@ class CommentTest < Test::Unit::TestCase
     ['  are preserved individually'],
     [''],
     []], c.lines)
+    
+    assert_equal "this is the target line", c.target_line
   end
   
   def test_parse
@@ -44,6 +47,7 @@ class CommentTest < Test::Unit::TestCase
  \t  # with whitespace   \t
 })
     assert_equal [['comment', 'spanning lines', 'with whitespace']], c.lines
+    assert_equal nil, c.target_line
   end
 
   def test_parse_accepts_string_scanner
@@ -53,6 +57,7 @@ class CommentTest < Test::Unit::TestCase
  \t  # with whitespace   \t
 }))
     assert_equal [['comment', 'spanning lines', 'with whitespace']], c.lines
+    assert_equal nil, c.target_line
   end
   
   def test_parse_treats_indented_lines_as_new_lines
@@ -64,6 +69,7 @@ class CommentTest < Test::Unit::TestCase
 # line
 })
     assert_equal [['comment'],[' with indented'], ["\tlines"], ['new spanning', 'line']], c.lines
+    assert_equal nil, c.target_line
   end
   
   def test_parse_preserves_newlines
@@ -79,6 +85,7 @@ class CommentTest < Test::Unit::TestCase
 # line
 })
     assert_equal [['comment'],[''],[''],[' with indented'],[''],["\tlines"],[''],['new spanning', 'line']], c.lines
+    assert_equal nil, c.target_line
   end
   
   def test_parse_stops_at_non_comment_line
@@ -89,6 +96,7 @@ class CommentTest < Test::Unit::TestCase
 # ignored
 })
     assert_equal [['comment', 'spanning lines']], c.lines
+    assert_equal nil, c.target_line
   end
 
   def test_parse_stops_when_block_returns_true
@@ -101,6 +109,113 @@ class CommentTest < Test::Unit::TestCase
   comment =~ /^end/
 end
     assert_equal [['comment', 'spanning lines']], c.lines
+    assert_equal nil, c.target_line
+  end
+  
+  def test_parse_sets_target_line_if_next_line_is_not_a_comment
+    c = Comment.parse(%Q{
+# comment
+target line
+ignored
+})
+    assert_equal [['comment']], c.lines
+    assert_equal "target line", c.target_line
+  end
+  
+  def test_parse_sets_target_line_as_next_non_comment_line
+    c = Comment.parse(%Q{
+# comment
+
+target line
+ignored
+})
+    assert_equal [['comment']], c.lines
+    assert_equal "target line", c.target_line
+  end
+  
+  def test_parse_does_not_set_target_line_if_comment_breaks_from_block
+    c = Comment.parse(%Q{
+# comment
+# end
+# ignored
+not the target line
+}) do |comment|
+  comment =~ /^end/
+end
+
+    assert_equal [['comment']], c.lines
+    assert_equal nil, c.target_line
+  end
+  
+  def test_parse_does_not_set_target_line_if_the_next_line_is_a_comment
+    c = Comment.parse(%Q{
+# comment
+
+# not a target line
+})
+
+    assert_equal [['comment']], c.lines
+    assert_equal nil, c.target_line
+  end
+  
+  def test_parse_just_parses_target_if_no_comments_are_given
+    c = Comment.parse(%Q{
+
+target line
+# ignored
+})
+
+    assert_equal [[]], c.lines
+    assert_equal 'target line', c.target_line
+  end
+  
+  def test_target_lines_may_contain_trailing_comments
+    c = Comment.parse(%Q{
+target line # with a trailing comment
+# ignored
+})
+
+    assert_equal [[]], c.lines
+    assert_equal 'target line # with a trailing comment', c.target_line
+  end
+  
+  def test_parse_can_handle_an_empty_or_whitespace_string_without_error
+    assert_nothing_raised { Comment.parse("") }
+    assert_nothing_raised { Comment.parse("\n   \t \r\n \f ") }
+  end
+  
+  #
+  # scan test
+  #
+  
+  def test_scan_documentation
+    lines = [
+      "# comments spanning multiple",
+      "# lines are collected",
+      "#",
+      "#   while indented lines",
+      "#   are preserved individually",
+      "#    ",
+      "not a comment line",
+      "# skipped since the loop breaks",
+      "# at the first non-comment line"]
+
+    c = Comment.new
+    lines.each do |line|
+      break unless Comment.scan(line) do |fragment|
+        c.push(fragment)
+      end
+    end
+     
+    actual = c.lines   
+    expected = [
+       ['comments spanning multiple', 'lines are collected'],
+       [''],
+       ['  while indented lines'],
+       ['  are preserved individually'],
+       [''],
+       []]
+    assert_equal(expected, actual)
   end
   
   #
@@ -113,31 +228,60 @@ end
   end
   
   #
-  # << test
+  # unshift test
+  #
+  
+  def test_unshift_documentation
+    c = Comment.new
+    c.unshift "some line"
+    c.unshift "fragments"
+    c.unshift ["a", "whole", "new line"]
+    assert_equal([["a", "whole", "new line"], ["fragments", "some line"]], c.lines)
+  end
+  
+  def test_unshift_unshifts_fragment_to_first_line
+    c.unshift "a line"
+    c.unshift "fragment"
+    assert_equal [["fragment", "a line"]], c.lines
+  end
+
+  def test_unshift_unshifts_array_if_given
+    c.unshift "fragment"
+    c.unshift ["some", "array"]
+    assert_equal [["some", "array"], ['fragment']], c.lines
+  end
+
+  def test_unshift_replaces_first_array_if_first_is_empty
+    c.unshift ["some", "array"]
+    assert_equal [["some", "array"]], c.lines
+  end
+  
+  #
+  # push test
   #
   
   def test_push_documentation
     c = Comment.new
-    c << "some line"
-    c << "fragments"
-    c << ["a", "whole", "new line"]
+    c.push "some line"
+    c.push "fragments"
+    c.push ["a", "whole", "new line"]
     assert_equal([["some line", "fragments"], ["a", "whole", "new line"]], c.lines)
   end
   
   def test_push_adds_fragment_to_last_line
-    c << "a line"
-    c << "fragment"
+    c.push "a line"
+    c.push "fragment"
     assert_equal [["a line", "fragment"]], c.lines
   end
 
   def test_push_adds_array_if_given
-    c << "fragment"
-    c << ["some", "array"]
+    c.push "fragment"
+    c.push ["some", "array"]
     assert_equal [['fragment'], ["some", "array"]], c.lines
   end
 
   def test_push_replaces_last_array_if_last_is_empty
-    c << ["some", "array"]
+    c.push ["some", "array"]
     assert_equal [["some", "array"]], c.lines
   end
   
@@ -146,10 +290,10 @@ end
   #
   
   def test_trim_removes_leading_and_trailing_empty_and_whitespace_lines
-    c << ['']
-    c << ["fragment"]
-    c << ['', "\t\r  \n", ' ']
-    c << []
+    c.push ['']
+    c.push ["fragment"]
+    c.push ['', "\t\r  \n", ' ']
+    c.push []
     
     assert_equal [[''],['fragment'],['', "\t\r  \n", ' '],[]], c.lines
     c.trim
@@ -157,8 +301,8 @@ end
   end
   
   def test_trim_ensures_lines_is_not_empty
-    c << ['']
-    c << ['']
+    c.push ['']
+    c.push ['']
     assert_equal [[''],['']], c.lines
     
     c.trim
@@ -170,24 +314,37 @@ end
   end
   
   #
+  # empty? test
+  #
+  
+  def test_empty_is_true_if_there_are_no_non_empty_lines_in_self
+    assert_equal [[]], c.lines
+    assert c.empty?
+    
+    c.lines.push "line"
+    
+    assert !c.empty?
+  end
+  
+  #
   # to_s test
   #
   
   def test_to_s_joins_lines_with_separators
-    c << "some line"
-    c << "fragments"
-    c << ["a", "whole", "new line"]
+    c.push "some line"
+    c.push "fragments"
+    c.push ["a", "whole", "new line"]
     
     assert_equal "some line.fragments:a.whole.new line", c.to_s('.', ':')
   end
   
   def test_to_s_wraps_lines_to_cols
-    c << "some line that will wrap"
+    c.push "some line that will wrap"
     assert_equal "some line\nthat will\nwrap", c.to_s(' ', "\n", 10)
   end
   
   def test_to_s_resolves_tabs_to_tabsize
-    c << "some\tresolved tab"
+    c.push "some\tresolved tab"
     assert_equal "some   resolved tab", c.to_s(' ', "\n", nil, 3)
   end
 end
