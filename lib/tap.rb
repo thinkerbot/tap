421a58f450fa0c41f2531e9f65dc25a9cedd5094
Simon Chiang <simon.a.chiang@gmail.com>
2008-12-05 13:41:10 -0700

    slight change to requires

--- lib/tap.rb
+++ lib/tap.rb
@@ -3,6 +3,7 @@ autoload(:YAML, 'yaml')                   # expensive to load
 $:.unshift File.expand_path(File.dirname(__FILE__))
 
 require 'rubygems'
+require 'configurable'
 require 'tap/constants'
 
 # require in order...
--- lib/tap/root.rb
+++ lib/tap/root.rb
@@ -1,4 +1,3 @@
-require 'configurable'
 require 'tap/support/versions'
 autoload(:FileUtils, 'fileutils')
 
