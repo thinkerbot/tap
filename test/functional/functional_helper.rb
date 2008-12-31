e723dbd9899c0a167459303cfa2b140f6ed1bf51
Simon Chiang <simon.a.chiang@gmail.com>
2008-12-31 09:20:17 -0700

    massive reorganization of files into modules

--- test/functional/functional_helper.rb
+++
@@ -1,5 +0,0 @@
-require File.join(File.dirname(__FILE__), '../../lib/tap')
-require 'test/unit'
-
-module Functional
-end
--- test/support/gems/rack_test.rb
+++
@@ -1,6 +0,0 @@
-require  File.join(File.dirname(__FILE__), '../../tap_test_helper')
-require 'tap/support/gems/rack'
-
-class RackTest < Test::Unit::TestCase
-
-end
