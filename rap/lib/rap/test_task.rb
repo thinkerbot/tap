3ef44bb369f2e87d3fc4dd2c16dd15560372a084
Simon Chiang <simon.a.chiang@gmail.com>
2009-01-04 11:25:08 -0700

    moved Rap::Tasks::Rake to Rap::Rake.  sketched out Rap::TestTask

---
+++ rap/lib/rap/test_task.rb
@@ -0,0 +1,54 @@
+require 'rap/declarations'
+
+module Rap
+  # ::manifest
+  class TestTask < Tap::Task
+    class << self
+      include Declarations
+      
+      def declare(name)
+      end
+    end
+    
+    include Tap::Support::ShellUtils
+    
+    config :libs, ['lib'], &c.list
+    config :pattern, "test/**/*_test.rb", &c.string
+    config :options, [], &c.list
+    config :verbose, true, &c.switch
+    config :warning, true, &c.switch
+    config :ruby_opts, [], &c.list
+    
+    # # Style of test loader to use. Options are:
+    # #
+    # # * :rake -- Rake provided test loading script (default).
+    # # * :direct -- Load tests using command line loader.
+    # #
+    # config :loader, :direct
+    
+    config :filter, ".", &c.regexp
+    
+    def test_files=(list)
+      @test_files = list
+    end
+    
+    def run_code
+      "-e 'ARGV.each{|f| load f}'"
+    end
+    
+    def test_files
+      @test_files ||= Dir.glob(pattern).select {|path| File.file?(path) && path =~ filter }
+    end
+    
+    def process
+      opts = libs.collect {|path| "-I\"#{File.expand_path(path)}\""}
+      opts << "-w" if warning
+      opts.concat(ruby_opts)
+      
+      files = test_files.collect {|path| "\"#{path}\""}
+      cmd = ["ruby", run_code] + opts + files
+      sh(cmd.join(' '))
+    end
+    
+  end
+end
