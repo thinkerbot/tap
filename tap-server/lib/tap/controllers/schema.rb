4d18a471e1ff51aed380e150bf61d47925feaa46
Simon Chiang <simon.a.chiang@gmail.com>
2009-03-27 15:30:18 -0600

    numerous commits to update schema with restful routes

--- tap-server/lib/tap/controllers/schema.rb
+++ tap-server/lib/tap/controllers/schema.rb
@@ -5,36 +5,78 @@ module Tap
     
     # ::controller
     class Schema < Tap::Controller
+      include RestRoutes
+      
       set :default_layout, 'layout.erb'
-  
-      # Initializes a new schema and redirects to display.
+      
+      # GET /projects
       def index
-        id = initialize_schema
-        redirect("/schema/display/#{id}")
-      end
-  
-      # Loads the schema indicated by id and renders 'schema.erb' with the default
-      # layout.
-      def display(id)
-        schema = load_schema(id)
-        render 'schema.erb', :locals => {
-          :id => id, 
-          :schema => schema
+        render 'index.erb', :locals => {
+          :schema => persistence.index
         }, :layout => true
       end
-  
+      
+      # GET /projects/id
+      def show(id=persistence.next_id)
+        extname = File.extname(id)
+        id = id.chomp(extname)
+        schema = load_schema(id)
+        
+        case extname
+        when '.txt'
+          response.headers['Content-Type'] = 'text/plain'
+          YAML.dump(schema.dump)
+        when '.yml'
+          response.headers['Content-Type'] = 'text/plain'
+          response['Content-Disposition'] = "attachment; filename=#{id}.yml;"
+          YAML.dump(schema.dump)
+        else
+          render 'schema.erb', :locals => {
+            :id => id,
+            :schema =>schema
+          }, :layout => true
+        end
+      end
+      
+      # 
+      #     # GET /projects/arg;edit/*args
+      #     def edit(arg, *args)...
+      
       # Updates the specified schema with the request parameters.  Update forwards
       # the request to the action ('add' or 'remove') specified in the action
       # parameter.
-      def update(id)
+      # POST /projects/*args
+      def create(*id)
         case request['action']
+        # rest actions
+        when 'update' then update(id)
+        when 'destroy' then destroy(id)
+        
+        # create actions
         when 'add'    then add(id)
         when 'remove' then remove(id)
         when 'echo'   then echo
         else raise Tap::ServerError, "unknown action: #{request['action']}"
         end
       end
-  
+      
+      # PUT /projects/*args
+      def update(id)
+        save_schema(id, request_schema)
+        redirect("/schema/#{id}")
+      end
+      
+      # DELETE /projects/*args
+      def destroy(id)
+        persistence.destroy(id)
+        redirect("/schema")
+      end
+        
+      ############################################################
+      # Helper Methods
+      ############################################################
+      protected
+      
       # Adds nodes or joins to the schema.  Parameters:
       #
       # nodes[]:: An array of nodes to add to the schema. Each entry is split using
@@ -57,19 +99,15 @@ module Tap
       # like '0:1')
       #
       def add(id)
-        unless request.post?
-          raise Tap::ServerError, "add must be performed with post"
-        end
-    
         round = (request['round'] || 0).to_i
         outputs = (request['outputs[]'] || []).collect {|index| index.to_i }
         inputs = (request['inputs[]'] || []).collect {|index| index.to_i }
         nodes = request['nodes[]'] || []
-    
+          
         load_schema(id) do |schema|
           nodes.each do |arg|
             next unless arg && !arg.empty?
-
+      
             outputs << schema.nodes.length
             schema.nodes << Tap::Support::Node.new(Shellwords.shellwords(arg), round)
           end
@@ -78,19 +116,13 @@ module Tap
             inputs.each {|index| schema[index].output = nil }
             outputs.each {|index| schema[index].input = round }
           else
-        
-            # temporary
-            if inputs.length > 1 && outputs.length > 1
-              raise "multi-way join specified"
-            end
-        
             schema.set(Tap::Support::Join, inputs, outputs)
           end
         end
-    
-        redirect("/schema/display/#{id}")
+        
+        redirect("/schema/#{id}")
       end
-  
+        
       # Removes nodes or joins from the schema.  Parameters:
       #
       # sources[]:: An array of source node indicies to remove.
@@ -108,14 +140,10 @@ module Tap
       # breaks the join '0:1' while 'sources[]=1' breaks the join '1:2'.
       #
       def remove(id)
-        unless request.post?
-          raise Tap::ServerError, "remove must be performed with post"
-        end
-    
         round = (request['round'] || 0).to_i
         outputs = (request['outputs[]'] || []).collect {|index| index.to_i }
         inputs = (request['inputs[]'] || []).collect {|index| index.to_i }
-    
+          
         load_schema(id) do |schema|
           # Remove joins.  Removed indicies are popped to ensure
           # that if a join was removed the node will not be.
@@ -138,124 +166,56 @@ module Tap
               false
             end
           end
-    
+          
           # Remove nodes. Setting a node to nil causes it's removal during 
           # compact; orphaned joins are removed during compact as well.
           (inputs & outputs).each do |index|
             schema.nodes[index] = nil
           end
         end
-    
-        redirect("/schema/display/#{id}")
+          
+        redirect("/schema/#{id}")
       end
-  
-      def submit(id)
-        case request['action']
-        when 'save'    then save(id)
-        when 'preview' then preview(id)
-        when 'echo'    then echo
-        when 'run'
-          dump_schema(id, schema)
-          run(id)
-        else raise Tap::ServerError, "unknown action: #{request['action']}"
-        end
+      
+      # helper to echo requests back... used to debug http requests
+      def echo
+        "<pre>#{request.params.to_yaml}</pre>"
       end
-  
-      def save(id)
-        unless request.post?
-          raise Tap::ServerError, "submit must be performed with post"
-        end
-    
-        dump_schema(id, schema)
-        redirect("/schema/display/#{id}")
-      end
-  
-      def preview(id)
-        response.headers['Content-Type'] = 'text/plain'
-        render('preview.erb', :locals => {:id => id, :schema => schema})
-      end
-  
-      def run(id)
-        unless request.post?
-          raise Tap::ServerError, "run must be performed with post"
-        end
-    
-        # it would be nice to someday put all this on a separate thread...
-        schema = load_schema(id)
-        tasks = server.env.tasks
-        schema.build(app) do |(key, *args)|
-          if const = tasks.search(key) 
-            const.constantize.parse(args, app) do |help|
-              raise "help not implemented"
-              #redirect("/app/help/#{key}")
-            end
-          else
-            raise ArgumentError, "unknown task: #{key}"
-          end
-        end
-    
-        Thread.new { app.run }
-        redirect("/app/tail")
-      end
-  
-      protected
-  
+      
       # Parses a Tap::Support::Schema from the request.
-      def schema
+      def request_schema
         argv = request['argv[]'] || []
         argv.delete_if {|arg| arg.empty? }
         Tap::Support::Schema.parse(argv)
       end
-  
-      def initialize_schema
-        current = root.glob(:schema, "*").collect {|path| File.basename(path).chomp(".yml") }
-    
-        id = random_key(current.length)
-        while current.include?(id)
-          id = random_key(current.length)
-        end
-    
-        dump_schema(id, schema)
-        id
-      end
-  
+      
       def load_schema(id)
-        unless path = root.filepath(:schema, "#{id}.yml")
-          raise ServerError, "no schema for id: #{id}"
+        schema = if persistence.has?(id)
+          Tap::Support::Schema.load_file(persistence.path(id))
+        else
+          Tap::Support::Schema.new
         end
-        schema = Tap::Support::Schema.load_file(path)
-    
+        
         if block_given?
           result = yield(schema)
-          dump_schema(id, schema)
+          save_schema(id, schema)
           result
         else
           schema
         end
       end
-  
-      def dump_schema(id, schema=nil)
-        root.prepare(:schema, "#{id}.yml") do |file|
+        
+      def save_schema(id, schema=nil)
+        persistence.update(id) do |file|
           file << YAML.dump(schema.dump) if schema
         end
       end
-  
+        
       def instantiate(*argv)
         key = argv.shift
         tasc = server.env.tasks.search(key).constantize 
         tasc.parse(argv)
-      end
-  
-      # helper to echo requests back... good for debugging
-      def echo # :nodoc:
-        "<pre>#{request.params.to_yaml}</pre>"
-      end
-  
-      # Generates a random integer key.
-      def random_key(length) # :nodoc:
-        length = 1 if length < 1
-        rand(length * 10000).to_s
-      end
+      end        
     end
   end
 end
