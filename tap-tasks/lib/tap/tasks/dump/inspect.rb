3c05b47ad8896d7f4ca175de99139b0b042786fa
Simon Chiang <simon.a.chiang@gmail.com>
2009-04-01 15:14:32 -0600

    added method config to inspect

--- tap-tasks/lib/tap/tasks/dump/inspect.rb
+++ tap-tasks/lib/tap/tasks/dump/inspect.rb
@@ -6,17 +6,23 @@ module Tap
       
       # :startdoc::manifest inspect and dump an object
       #
-      # Dumps objects to a file or IO using object.inspect.  See the default
-      # tap dump task for more details.
+      # Dumps objects to a file or IO using object.inspect.  An alternate
+      # method can be specified for inspection using the inspect_method
+      # config.  See the default tap dump task for more details.
       #
       #   % tap run -- load/yaml "{key: value}" --: inspect
       #   {"key"=>"value"}
       #
+      #   % tap run -- load string --: inspect -m length
+      #   6
+      #
       class Inspect < Tap::Dump
+        
+        config :inspect_method, 'inspect', :short => :m    # The inspection method
         
         # Dumps the object to io using obj.inspect
         def dump(obj, io)
-          io.puts obj.inspect
+          io.puts obj.send(inspect_method)
         end
       end
     end
