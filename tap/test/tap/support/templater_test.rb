620c65501dc31a7cc91378fde9502259314ce7bd
Simon Chiang <simon.a.chiang@gmail.com>
2009-04-11 15:31:10 -0600

    enabled tests for support modules

--- tap/test/tap/support/templater_test.rb
+++ tap/test/tap/support/templater_test.rb
@@ -3,10 +3,12 @@ require 'tap/support/templater'
 
 class TemplaterTest < Test::Unit::TestCase
   include Tap::Support
-  acts_as_subset_test
   
-  condition(:ruby_1_8) { RUBY_VERSION =~ /^1.8/ }
-  condition(:ruby_1_9) { RUBY_VERSION =~ /^1.9/ }
+  def version_test(version)
+    if RUBY_VERSION =~ version
+      yield
+    end
+  end
   
   def test_documentation
     t = Templater.new( "key: <%= value %>")
@@ -17,8 +19,8 @@ class TemplaterTest < Test::Unit::TestCase
     assert_equal "key: another", t.build
     
     e = ERB.new("<%= 1 + 2 %>")
-    condition_test(:ruby_1_8) { assert_equal("_erbout = ''; _erbout.concat(( 1 + 2 ).to_s); _erbout", e.src) }
-    condition_test(:ruby_1_9) { assert_equal("#coding:US-ASCII\n_erbout = ''; _erbout.concat(( 1 + 2 ).to_s); _erbout.force_encoding(__ENCODING__)", e.src) }
+    version_test(/^1\.8/) { assert_equal("_erbout = ''; _erbout.concat(( 1 + 2 ).to_s); _erbout", e.src) }
+    version_test(/^1\.9/) { assert_equal("#coding:US-ASCII\n_erbout = ''; _erbout.concat(( 1 + 2 ).to_s); _erbout.force_encoding(__ENCODING__)", e.src) }
     
     template = %Q{
 # Un-nested content
@@ -62,114 +64,5 @@ end}
     
     t = Templater.new erb, {:attr => 'value'}
     assert_equal "key: valuevalue", t.build
-  end
-
-end
-
-class TemplaterUtilsTest < Test::Unit::TestCase
-  include Tap::Support::Templater::Utils
-  include Tap::Test::SubsetTest
-  
-  #
-  # yamlize test
-  #
-  
-  def test_yamlize_returns_to_yaml_minus_header_and_newline
-    assert_equal "key: value", yamlize({'key' => 'value'})
-    assert_equal({'key' => 'value'}, YAML.load(yamlize({'key' => 'value'})))
-    
-    assert_equal "{}", yamlize({})
-    assert_equal({}, YAML.load(yamlize({})))
-    
-    assert_equal "~", yamlize(nil)
-    assert_equal nil, YAML.load(yamlize(nil))
-    
-    assert_equal "- 1\n- 2\n- 3", yamlize([1, 2, 3])
-    assert_equal [1, 2, 3], YAML.load(yamlize([1, 2, 3]))
-    
-    assert_equal "[]", yamlize([])
-    assert_equal [], YAML.load(yamlize([]))
-    
-    assert_equal "|-\nsome\nmultiline\nstring", yamlize("some\nmultiline\nstring")
-    assert_equal "some\nmultiline\nstring", YAML.load(yamlize("some\nmultiline\nstring"))
-    
-    assert_equal '""', yamlize("")
-    assert_equal "", YAML.load(yamlize(""))
-  end
-  
-  #
-  # nest test
-  #
-  
-  def test_nest_documentation
-    result = nest([["\nmodule Some", "end\n"],["module Nested", "end"]]) { "class Const\nend" }
-    expected = %Q{
-module Some
-  module Nested
-    class Const
-    end
-  end
-end
-}
-    
-    assert_equal expected, result
-  end
-  
-  def test_nest_nests_content_in_nesting_constant
-    content = "multiline\ncontent"
-    nested_content = %Q{
-module Sample
-  module Nest
-    multiline
-    content
-  end
-end
-}.strip
-
-    assert_equal nested_content, nest([['module Sample', 'end'], ['module Nest', 'end']]) { content }
-    assert_equal content, nest([]) { content }
-  end
-  
-  def test_nest_speed
-    benchmark_test do |x|
-      content = "some content\n" * 100
-      nesting = [['module Sample', 'end'], ['module Nest', 'end']]
-      
-      n = 1000
-      x.report("#{n}x nest") { n.times { nest(nesting) {content} } }
-    end
-  end
-  
-  #
-  # module_nest test
-  #
-  
-  def test_module_nest_documentation
-    result = module_nest('Some::Nested') { "class Const\nend" }
-    expected = %Q{
-module Some
-  module Nested
-    class Const
-    end
-  end
-end
-}.strip
-
-    assert_equal expected, result
-  end
-  
-  def test_module_nest_nests_content_in_nesting_module
-    content = "multiline\ncontent"
-    nested_content = %Q{
-module Sample
-  module Nest
-    multiline
-    content
-  end
-end
-}.strip
-
-    assert_equal nested_content, module_nest('Sample::Nest') { content }
-    assert_equal content, module_nest('') { content }
   end
 end
--- tap/test/tap_test_helper.rb
+++ tap/test/tap_test_helper.rb
@@ -1,97 +1,98 @@
+require 'test/unit'
 # setup testing with submodules
-begin
-  require File.join(File.dirname(__FILE__), '../lib/tap')
-rescue(LoadError)
-  puts %Q{
-Tests probably cannot be run because the submodules have
-not been initialized.  Use these commands and try again:
+# begin
+#   require File.join(File.dirname(__FILE__), '../lib/tap')
+# rescue(LoadError)
+#   puts %Q{
+# Tests probably cannot be run because the submodules have
+# not been initialized.  Use these commands and try again:
+# 
+#   % git submodule init
+#   % git submodule update
+# 
+# }
+#   raise
+# end
 
-  % git submodule init
-  % git submodule update
-
-}
-  raise
-end
-
-require 'tap/test'
-
-unless defined?(ObjectWithExecute)
-  class ObjectWithExecute
-    def execute(input)
-      input
-    end
-  end
-
-  class Tracer
-    include Tap::Support::Executable
-
-    class << self
-      def intern(n, runlist, &block)
-        Array.new(n) { |index| new(index, runlist, &block) }
-      end
-    end
-
-    def initialize(index, runlist, &block)
-      @index = index
-      @runlist = runlist
-
-      @app = Tap::App.instance
-      @method_name = :trace
-      @on_complete_block =nil
-      @dependencies = []
-      @block = block || lambda {|task, str| task.mark(str) }
-    end
-
-    def id
-      @index.to_s
-    end
-    
-    def mark(input)
-      "#{input} #{id}".strip
-    end
-    
-    def inspect
-      "Tracer(#{@index})"
-    end
-
-    def trace(*inputs)
-      @runlist << id
-      @block.call(self, *inputs)
-    end
-  end
-  
-  # Some convenience methods used in testing tasks, workflows, app, etc.
-  module TapTestMethods # :nodoc:
-    attr_accessor  :runlist
-    
-    # Setup clears the test using clear_tasks and assures that Tap::App.instance 
-    # is the test-specific application.
-    def setup
-      super
-      clear_runlist
-    end
-
-    # Clears the runlist.
-    def clear_runlist
-      # clear the attributes
-      @runlist = []
-    end
-
-    # A tracing procedure.  echo adds input to runlist then returns input.
-    def echo
-      lambda do |task, *inputs| 
-        @runlist << inputs
-        inputs
-      end
-    end
-    
-    # A tracing procedure for numeric inputs.  add_one adds the input to 
-    # runlist then returns input + 1.
-    def add_one
-      lambda do |task, input| 
-        @runlist << input
-        input += 1
-      end
-    end
-  end
-end
+# require 'tap/test'
+# 
+# unless defined?(ObjectWithExecute)
+#   class ObjectWithExecute
+#     def execute(input)
+#       input
+#     end
+#   end
+# 
+#   class Tracer
+#     include Tap::Support::Executable
+# 
+#     class << self
+#       def intern(n, runlist, &block)
+#         Array.new(n) { |index| new(index, runlist, &block) }
+#       end
+#     end
+# 
+#     def initialize(index, runlist, &block)
+#       @index = index
+#       @runlist = runlist
+# 
+#       @app = Tap::App.instance
+#       @method_name = :trace
+#       @on_complete_block =nil
+#       @dependencies = []
+#       @block = block || lambda {|task, str| task.mark(str) }
+#     end
+# 
+#     def id
+#       @index.to_s
+#     end
+#     
+#     def mark(input)
+#       "#{input} #{id}".strip
+#     end
+#     
+#     def inspect
+#       "Tracer(#{@index})"
+#     end
+# 
+#     def trace(*inputs)
+#       @runlist << id
+#       @block.call(self, *inputs)
+#     end
+#   end
+#   
+#   # Some convenience methods used in testing tasks, workflows, app, etc.
+#   module TapTestMethods # :nodoc:
+#     attr_accessor  :runlist
+#     
+#     # Setup clears the test using clear_tasks and assures that Tap::App.instance 
+#     # is the test-specific application.
+#     def setup
+#       super
+#       clear_runlist
+#     end
+# 
+#     # Clears the runlist.
+#     def clear_runlist
+#       # clear the attributes
+#       @runlist = []
+#     end
+# 
+#     # A tracing procedure.  echo adds input to runlist then returns input.
+#     def echo
+#       lambda do |task, *inputs| 
+#         @runlist << inputs
+#         inputs
+#       end
+#     end
+#     
+#     # A tracing procedure for numeric inputs.  add_one adds the input to 
+#     # runlist then returns input + 1.
+#     def add_one
+#       lambda do |task, input| 
+#         @runlist << input
+#         input += 1
+#       end
+#     end
+#   end
+# end
