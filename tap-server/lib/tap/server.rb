8e6f7bf41985b4d22a13502e927b6d4ca31fdfc3
Simon Chiang <simon.a.chiang@gmail.com>
2009-05-03 10:33:39 -0600

    extracted Base out of Server

--- tap-server/lib/tap/server.rb
+++ tap-server/lib/tap/server.rb
@@ -1,7 +1,5 @@
-require 'rack'
-require 'rack/mock'
-
 require 'tap'
+require 'tap/server/base'
 require 'tap/server/session'
 require 'tap/server/server_error'
 
@@ -94,13 +92,10 @@ module Tap
     end
     
     include Utils
+    include Base
     include Rack::Utils
-    include Configurable
     
     config :mode, :development, &c.select(:development, :production, &c.symbol)
-    config :servers, %w[thin mongrel webrick], &c.list  # a list of preferred handlers
-    config :host, 'localhost', &c.string                # the server host
-    config :port, 8080, &c.integer                      # the server port
     config :use_multiple_sessions, false
     
     # A hash of (key, controller) pairs mapping the controller part of a route
@@ -132,29 +127,6 @@ module Tap
       @cache = {}
       @handler = nil
       initialize_config(config)
-    end
-    
-    # Runs self as configured, on the specified server, host, and port.  Use an
-    # INT signal to interrupt.
-    def run!(handler=rack_handler)
-      #app.log :run, "#{host}:#{port} (#{handler})"
-      handler.run self, :Host => host, :Port => port do |handler_instance|
-        @handler = handler_instance
-        trap(:INT) { stop! }
-      end
-    end
-    
-    # Stops the server if running (ie a handler is set).  Returns true if the
-    # server was stopped, and false otherwise.
-    def stop!
-      if handler
-        # Use thins' hard #stop! if available, otherwise just #stop
-        handler.respond_to?(:stop!) ? handler.stop! : handler.stop
-        @handler = nil
-        false
-      else
-        true
-      end
     end
     
     def initialize_session
@@ -274,20 +246,6 @@ module Tap
     # Returns true if mode is :development.
     def development? # :nodoc:
       mode == :development
-    end
-    
-    # Looks up and returns the first available Rack::Handler as listed in the
-    # servers configuration. (Note rack_handler returns a handler class, not
-    # an instance).  Adapted from Sinatra.detect_rack_handler
-    def rack_handler # :nodoc:
-      servers.each do |server_name|
-        begin
-          return Rack::Handler.get(server_name)
-        rescue LoadError
-        rescue NameError
-        end
-      end
-      raise "Server handler (#{servers.join(',')}) not found."
     end
   end
 end
---
+++ tap-server/lib/tap/server/base.rb
@@ -0,0 +1,52 @@
+require 'rack'
+require 'configurable'
+
+module Tap
+  class Server
+    module Base
+      include Configurable
+    
+      config :servers, %w[thin mongrel webrick], &c.list  # a list of preferred handlers
+      config :host, 'localhost', &c.string                # the server host
+      config :port, 8080, &c.integer                      # the server port
+    
+      # Runs self as configured, on the specified server, host, and port.  Use an
+      # INT signal to interrupt.
+      def run!(handler=rack_handler)
+        handler.run self, :Host => host, :Port => port do |handler_instance|
+          @handler = handler_instance
+          trap(:INT) { stop! }
+        end
+      end
+    
+      # Stops the server if running (ie a handler is set).  Returns true if the
+      # server was stopped, and false otherwise.
+      def stop!
+        if handler
+          # Use thins' hard #stop! if available, otherwise just #stop
+          handler.respond_to?(:stop!) ? handler.stop! : handler.stop
+          @handler = nil
+          false
+        else
+          true
+        end
+      end
+    
+      protected
+    
+      # Looks up and returns the first available Rack::Handler as listed in the
+      # servers configuration. (Note rack_handler returns a handler class, not
+      # an instance).  Adapted from Sinatra.detect_rack_handler
+      def rack_handler # :nodoc:
+        servers.each do |server_name|
+          begin
+            return Rack::Handler.get(server_name)
+          rescue LoadError
+          rescue NameError
+          end
+        end
+        raise "Server handler (#{servers.join(',')}) not found."
+      end
+    end
+  end
+end
