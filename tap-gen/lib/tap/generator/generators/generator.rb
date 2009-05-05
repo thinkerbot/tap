6219bdadffe49f2d63a10ea90c942b45489d03f2
Simon Chiang <simon.a.chiang@gmail.com>
2009-05-05 14:21:21 -0600

    updated generator generator

--- tap-gen/lib/tap/generator/generators/generator.rb
+++ tap-gen/lib/tap/generator/generators/generator.rb
@@ -1,23 +1,18 @@
-require 'tap/env'
+require 'tap/generator/generators/task'
 
 module Tap::Generator::Generators
   
   # :startdoc::generator a generator task and test
   #
   # Generates a new generator.
-  class Generator < Tap::Generator::Base
+  class Generator < Tap::Generator::Generators::Task
     
     config :test, true, &c.switch  # specifies creation of a test file
     
     def manifest(m, const_name)
+      super
+      
       const = Tap::Env::Constant.new(const_name.camelize)
-      dir = path('lib', const.path)
-      
-      # make the directory
-      m.directory dir
-      
-      # make the generator
-      m.template path(dir, "#{const.basename}_generator.rb"), "task.erb", :const => const
       
       # make the templates directory
       m.directory path('templates', const.path)
@@ -27,12 +22,6 @@ module Tap::Generator::Generators
       # added, and would have to be escaped in a template)
       m.file path('templates', const.path, 'template_file.erb') do |file|
         file << "# A sample template file.\nkey: <%= key %>\n"
-      end
-      
-      if test
-        test_path = path('test', "#{const.path}_generator_test.rb")
-        m.directory File.dirname(test_path)
-        m.template test_path, "test.erb", :const => const
       end
     end
   end
