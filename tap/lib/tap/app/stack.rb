2a49a89de48936a3c245160e63e2b8cd662c8947
Simon Chiang <simon.a.chiang@gmail.com>
2009-05-04 20:23:04 -0600

    updated stack doc

--- tap/lib/tap/app/stack.rb
+++ tap/lib/tap/app/stack.rb
@@ -1,6 +1,13 @@
 module Tap
   class App
+    
+    # The base of the application call stack.
     class Stack
+      
+      # Calls the node with the inputs:
+      #
+      #   node.call(*inputs)
+      #
       def call(node, inputs)
         node.call(*inputs)
       end
