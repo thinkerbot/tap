34f405979f9f1c643f40eb316ada426f607fbfb2
Simon Chiang <simon.a.chiang@gmail.com>
2009-06-15 07:07:17 -0600

    sketched out debugger

--- tap/lib/tap/middleware.rb
+++ tap/lib/tap/middleware.rb
@@ -37,6 +37,17 @@ module Tap
       initialize_config(config)
     end
     
+    # Returns the app at the base of the stack.
+    def app
+      @app ||= begin
+        current = stack
+        until current.kind_of?(App::Stack)
+          current = current.stack
+        end
+        current.app
+      end
+    end
+    
     # By default call simply calls stack with the node and inputs.
     def call(node, inputs=[])
       stack.call(node, inputs)
