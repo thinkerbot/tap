6f291774a889fdd1e8bd1c27e2a15699bb02c663
Simon Chiang <simon.a.chiang@gmail.com>
2009-06-22 21:56:48 -0600

    moved version information to separate file (rap)

--- rap/lib/rap.rb
+++ rap/lib/rap.rb
@@ -1,13 +1,7 @@
 require 'tap'
 require 'rap/declarations'
+require 'rap/version'
 
 module Rap
   autoload(:Rake, 'rap/rake')
-
-  MAJOR = 0
-  MINOR = 14
-  TINY = 0
-  
-  VERSION="#{MAJOR}.#{MINOR}.#{TINY}"
-  WEBSITE="http://tap.rubyforge.org/rap"
 end
---
+++ rap/lib/rap/version.rb
@@ -0,0 +1,8 @@
+module Rap
+  MAJOR = 0
+  MINOR = 14
+  TINY = 0
+  
+  VERSION="#{MAJOR}.#{MINOR}.#{TINY}"
+  WEBSITE="http://tap.rubyforge.org/rap"
+end
