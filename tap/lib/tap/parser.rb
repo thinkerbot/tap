56e213224785e0ae8020a003c96bc97c88d11f66
Simon Chiang <simon.a.chiang@gmail.com>
2009-08-17 12:22:41 -0600

    added next generation parser

---
+++ tap/lib/tap/parser.rb
@@ -0,0 +1,266 @@
+require 'shellwords'
+
+module Tap
+  
+  # A parser for workflow schema defined on the command line.
+  #
+  # == Syntax
+  #
+  # The command line syntax can be thought of as a series of ARGV arrays
+  # connected by breaks.  The arrays define tasks in a workflow while the
+  # breaks define joins and middleware.  These are the available breaks:
+  #
+  #   break          meaning
+  #   --             default delimiter
+  #   --:            sequence join
+  #   --[][]         join (ex: sequence, fork, merge)
+  #   --/            middleware
+  #   --.            generic resource
+  #
+  # As an example, this defines three tasks (a, b, c) and sequences the
+  # b and c tasks:
+  #
+  #   Schema.parse("a -- b --: c").workflow
+  #   # => [
+  #   # [0, "task", "a"],
+  #   # [1, "task", "b"],
+  #   # [2, "task", "c"],
+  #   # [nil, "join", "join", [1], [2]]
+  #   # ]
+  #
+  # ==== Escapes and End Flags
+  #
+  # Breaks can be escaped by enclosing them in '-.' and '.-' delimiters;
+  # any number of arguments may be enclosed within the escape. After the 
+  # end delimiter, breaks are active once again.
+  #
+  #   Schema.parse("a -. -- b .- -- c").workflow
+  #   # => [
+  #   # [0, "task", "a", "--", "b"],
+  #   # [1, "task", "c"]
+  #   # ]
+  #
+  # Parsing continues until the end of argv, or a an end flag '---' is 
+  # reached.  The end flag may also be escaped.
+  #
+  #   Schema.parse("a -- b --- c").workflow
+  #   # => [
+  #   # [0, "task", "a"],
+  #   # [1, "task", "b"]
+  #   # ]
+  #
+  class Parser
+    
+    # The escape begin argument
+    ESCAPE_BEGIN = "-."
+
+    # The escape end argument
+    ESCAPE_END = ".-"
+
+    # The parser end flag
+    END_FLAG = "---"
+  
+    # Matches any breaking arg. Examples:
+    #
+    #   --
+    #   --:
+    #   --[1,2][3]
+    #   --@
+    #   --/var
+    #   --.
+    #
+    # After the match:
+    #
+    #   $1:: The string after the break, or nil
+    #        (ex: '--' => nil, '--:' => ':', '--[1,2][3,4]' => '[1,2][3,4]')
+    #
+    BREAK =  /\A--(?:\z|([\:\[\/\.@].*?)\z)/
+  
+    # Matches a sequence break. After the match:
+    #
+    #   $1:: The modifier string, or nil
+    #        (ex: ':' => nil, ':i' => 'i')
+    #
+    SEQUENCE = /\A:(.+)?\z/
+  
+    # Matches a generic join break. After the match:
+    #
+    #   $1:: The inputs string.
+    #        (ex: '[1,2,3][4,5,6]' => '1,2,3')
+    #   $2:: The outputs string.
+    #        (ex: '[1,2,3][4,5,6]' => '4,5,6')
+    #   $3:: The modifier string, or nil
+    #        (ex: '[][]is' => 'is')
+    #
+    JOIN = /\A\[([\d,]*)\]\[([\d,]*)\](.+)?\z/
+  
+    # Matches a join modifier. After the match:
+    #
+    #   $1:: The modifier flag string.
+    #        (ex: 'is.sync' => 'is')
+    #   $2:: The class string.
+    #        (ex: 'is.sync' => 'sync')
+    #
+    JOIN_MODIFIER = /\A([A-z]*)(?:\.(.*))?\z/
+  
+    # Matches a signal break. After the match:
+    #
+    #   $1:: The modifier string, or nil
+    #        (ex: '/var' => 'var')
+    #
+    SIGNAL = /\A\/(.+)?\z/
+  
+    # Matches a spec break.
+    SPEC = "."
+    
+    # Matches a job break.
+    JOB = "@"
+  
+    attr_reader :specs
+    
+    def initialize(specs=[])
+      @specs = specs
+    end
+    
+    def parse(argv)
+      argv = argv.dup unless argv.kind_of?(String)
+      parse!(argv)
+    end
+
+    # Same as parse, but removes parsed args from argv.
+    def parse!(argv)
+      argv = Shellwords.shellwords(argv) if argv.kind_of?(String)
+      return argv if argv.empty?
+      
+      unless argv[0] =~ BREAK
+        argv.unshift("--") 
+      end
+      
+      @current_index = -1
+      @current = nil
+      escape = false
+      
+      while !argv.empty?
+        arg = argv.shift
+
+        # if escaping, add escaped arguments 
+        # until an escape-end argument
+        if escape
+          if arg == ESCAPE_END
+            escape = false
+          else
+            current << arg
+          end
+          next
+        end
+      
+        # handle breaks and parser flags
+        case arg
+        when BREAK
+          begin
+            @current_index += 1
+            @current = parse_break($1)
+          rescue
+            raise "invalid break: #{arg.inspect} (#{$!.message})"
+          end
+          next
+
+        when ESCAPE_BEGIN
+          escape = true
+          next
+
+        when END_FLAG
+          break
+        
+        end if arg[0] == ?-
+      
+        # add all remaining args to the current argv
+        current << arg
+      end
+      
+      @current_index = nil
+      @current = nil
+      
+      argv
+    end
+  
+    private
+    
+    def spec(*argv) # :nodoc:
+      specs << argv
+      argv
+    end
+    
+    # returns the current argv or a task argv for the current index
+    def current # :nodoc:
+      @current ||= spec(@current_index.to_s, 'task')
+    end
+  
+    # determines the type of break and modifies self appropriately
+    def parse_break(one) # :nodoc:
+      case one
+      when nil
+      when JOB
+        parse_job
+      when SEQUENCE
+        parse_sequence($1)
+      when JOIN
+        parse_join($1, $2, $3)
+      when SIGNAL
+        parse_signal($1)
+      when SPEC
+        parse_spec
+      else
+        raise "invalid modifier"
+      end
+    end
+    
+    # parses the match of a SEQUENCE regexp
+    def parse_sequence(one) # :nodoc:
+      @current = nil
+      argv = current
+      parse_join_spec(one, "#{@current_index - 1}", @current_index.to_s)
+      argv
+    end
+    
+    # parses the match of a JOIN regexp
+    def parse_join(one, two, three) # :nodoc:
+      parse_join_spec(three, one, two)
+    end
+        
+    # parses a join modifier string into an argv.
+    def parse_join_spec(modifier, inputs, outputs) # :nodoc:
+      argv = [nil, 'join']
+      
+      case 
+      when modifier.nil?
+        argv << 'join'
+        argv << inputs
+        argv << outputs
+      when modifier =~ JOIN_MODIFIER
+        argv << ($2 || 'join')
+        argv << inputs
+        argv << outputs
+        $1.split("").each {|char| argv << "-#{char}"}
+      else
+        raise "invalid join modifier"
+      end
+      
+      specs << argv
+      argv
+    end
+        
+    # parses the match of a SIGNAL regexp
+    def parse_signal(one) # :nodoc:
+      spec(one, nil)
+    end
+    
+    def parse_spec # :nodoc:
+      spec(@current_index.to_s)
+    end
+    
+    def parse_job # :nodoc:
+      spec(nil, nil, 'enque')
+    end
+  end
+end
---
+++ tap/test/tap/parser_test.rb
@@ -0,0 +1,372 @@
+require File.join(File.dirname(__FILE__), '../tap_test_helper')
+require 'tap/parser'
+
+class ParserTest < Test::Unit::TestCase
+  Parser = Tap::Parser
+  
+  attr_accessor :parser
+  
+  def setup
+    super
+    @parser = Parser.new  
+  end
+  
+#   #
+#   # doucmentation test
+#   #
+#   
+#   def test_parse_documentation
+#     assert_equal [
+#       ["0", "task", "a"],
+#       ["1", "task", "b"],
+#       ["2", "task", "c"],
+#       [nil, "join", "join", ["1"], ["2"]],
+#     ], Tap::Schema.parse("a -- b --: c").specs
+#     
+#     assert_equal [
+#       ["0", "task", "a", "--", "b"],
+#       ["1", "task", "c"]
+#     ], Tap::Schema.parse("a -. -- b .- -- c").specs
+#     
+#     assert_equal [
+#       ["0", "task", "a"],
+#       ["1", "task", "b"]
+#     ], Tap::Schema.parse("a -- b --- c").specs
+#   end
+#   
+  #
+  # BREAK test
+  #
+  
+  def test_BREAK_regexp
+    r = Parser::BREAK
+    
+    # break
+    assert "--" =~ r
+    assert_equal nil, $1
+    
+    # sequence join
+    assert "--:" =~ r
+    assert_equal ":", $1
+    
+    assert "--:is" =~ r
+    assert_equal ":is", $1
+    
+    # general join
+    assert "--[]" =~ r
+    assert_equal "[]", $1
+        
+    assert "--[1,2][3,4]is.join" =~ r
+    assert_equal "[1,2][3,4]is.join", $1
+    
+    # enq
+    assert "--@" =~ r
+    assert_equal "@", $1
+    
+    # init
+    assert "--." =~ r
+    assert_equal ".", $1
+    
+    # signal
+    assert "--/var" =~ r
+    assert_equal "/var", $1
+    
+    # non-matching
+    assert "goodnight" !~ r
+    assert "moon" !~ r
+    assert "8" !~ r
+    
+    assert "-" !~ r
+    assert " -- " !~ r
+    assert "-- " !~ r
+    assert " --" !~ r
+    
+    assert "-o" !~ r
+    assert "-opt" !~ r
+    assert "--opt" !~ r
+    assert "--no-opt" !~ r
+    
+    # escapes
+    assert "---" !~ r
+    assert "-." !~ r
+    assert ".-" !~ r
+  end
+
+  #
+  # SEQUENCE test
+  #
+  
+  def test_SEQUENCE_regexp
+    r = Parser::SEQUENCE
+    
+    assert ":" =~ r
+    assert_equal nil, $1
+  
+    assert ":i" =~ r
+    assert_equal "i", $1
+    
+    assert ":is.join" =~ r
+    assert_equal "is.join", $1
+  end
+  
+  #
+  # JOIN test
+  #
+  
+  def test_JOIN_regexp
+    r = Parser::JOIN
+
+    assert "[][]" =~ r
+    assert_equal "", $1
+    assert_equal "", $2
+    assert_equal nil, $3
+    
+    assert "[1,2,3][4,5,6]is.join" =~ r
+    assert_equal "1,2,3", $1
+    assert_equal "4,5,6", $2
+    assert_equal "is.join", $3
+    
+    # input/output variations
+    assert "[1][2]" =~ r
+    assert_equal "1", $1
+    assert_equal "2", $2
+    
+    # non-matching
+    assert "1[2]" !~ r
+    assert "[1]2" !~ r
+  end
+  
+  #
+  # JOIN_MODIFIER test
+  #
+  
+  def test_JOIN_MODIFIER_regexp
+    r = Parser::JOIN_MODIFIER
+    
+    assert "i" =~ r
+    assert_equal "i", $1
+    assert_equal nil, $2
+    
+    assert "is" =~ r
+    assert_equal "is", $1
+    assert_equal nil, $2
+    
+    assert "is.sync" =~ r
+    assert_equal "is", $1
+    assert_equal "sync", $2
+    
+    assert ".sync" =~ r
+    assert_equal "", $1
+    assert_equal "sync", $2
+    
+    assert "A.sync" =~ r
+    assert_equal "A", $1
+    assert_equal "sync", $2
+    
+    assert "is." =~ r
+    assert_equal "is", $1
+    assert_equal "", $2
+    
+    # non-matching cases
+    assert " join -i -s" !~ r
+    assert " " !~ r
+    assert "1" !~ r
+  end
+  
+  #
+  # SIGNAL test
+  #
+  
+  def test_SIGNAL_regexp
+    r = Parser::SIGNAL
+    
+    assert "/var" =~ r
+    assert_equal "var", $1
+    
+    assert "/" =~ r
+    assert_equal nil, $1
+    
+    # non-matching
+    assert "str" !~ r
+  end
+  
+  #
+  # parse test
+  #
+  
+  def test_parse_parses_task_and_enque_along_option_break
+    parser.parse %w{-- a b c -- x y z}
+    assert_equal [
+      ["0", "task", "a", "b", "c"],
+      ["1", "task", "x", "y", "z"]
+    ], parser.specs
+  end
+  
+  def test_parse_unshifts_option_break_if_argv_does_not_start_with_a_break
+    parser.parse %w{a b c}
+    assert_equal [
+      ["0", "task", "a", "b", "c"],
+    ], parser.specs
+  end
+  
+  def test_parse_allows_options_in_tasks
+    parser.parse %w{-- a -b --c}
+    assert_equal [
+      ["0", "task", "a", "-b", "--c"],
+    ], parser.specs
+  end
+  
+  def test_parse_incrementes_index_but_does_not_add_tasks_for_empty_breaks
+    parser.parse %w{-- a -- -- -- b --}
+    assert_equal [
+      ["0", "task", "a"],
+      ["3", "task", "b"]
+    ], parser.specs
+  end
+  
+  def test_parse_is_non_destructive
+    argv = %w{-- a b c}
+    assert_equal [], parser.parse(argv)
+    assert_equal %w{-- a b c}, argv
+  end
+  
+  def test_parse_does_not_parse_escaped_args
+    parser.parse %w{-- a -. -- --: -z- .- b -- c}
+    assert_equal [
+      ["0", "task", "a", "--", "--:", "-z-", "b"],
+      ["1", "task", "c"]
+    ], parser.specs
+  end
+  
+  def test_parse_stops_at_end_flag
+    parser.parse %w{-- a --- -- b}
+    assert_equal [
+      ["0", "task", "a"]
+    ], parser.specs
+  end
+  
+  def test_parse_returns_remaining_args
+    assert_equal [], parser.parse(%w{-- a -- b})
+    assert_equal ["b"], parser.parse(%w{-- a --- b})
+  end
+  
+  def test_parse_splits_string_to_argv
+    assert_equal ['c'], parser.parse("a -- b --- c")
+    assert_equal [
+      ["0", "task", "a"],
+      ["1", "task", "b"]
+    ], parser.specs
+  end
+  
+  def test_parse_resets_counter_and_appends_specs_for_each_call
+    parser.parse %w{-- a b c}
+    parser.parse %w{-- x y z}
+    assert_equal [
+      ["0", "task", "a", "b", "c"],
+      ["0", "task", "x", "y", "z"]
+    ], parser.specs
+  end
+  
+  def test_parse_raises_error_for_invalid_breaks
+    err = assert_raises(RuntimeError) { parser.parse "--[]" }
+    assert_equal "invalid break: \"--[]\" (invalid modifier)", err.message
+    
+    err = assert_raises(RuntimeError) { parser.parse "--[][]123" }
+    assert_equal "invalid break: \"--[][]123\" (invalid join modifier)", err.message
+  end
+  
+  #
+  # parse! test
+  #
+  
+  def test_parse_bang_is_destructive
+    argv = %w{-- a b c}
+    assert_equal [], parser.parse!(argv)
+    assert_equal [], argv
+  end
+  
+  #
+  # sequence test
+  #
+  
+  def test_sequence_breaks_assign_sequence_joins
+    parser.parse "-- a --: b --: c"
+    assert_equal [
+      ["0", "task", "a"],
+      ["1", "task", "b"],
+      [nil, "join", "join", "0", "1"],
+      ["2", "task", "c"],
+      [nil, "join", "join", "1", "2"]
+    ], parser.specs
+  end
+  
+  def test_sequence_with_modifier
+    parser.parse  "-- a --:is.class b"
+    assert_equal [
+      ["0", "task", "a"],
+      ["1", "task", "b"],
+      [nil, "join", "class", "0", "1", "-i", "-s"],
+    ], parser.specs
+  end
+  
+  #
+  # join test
+  # 
+  
+  def test_parser_parses_joins
+    parser.parse "--[1][2] --[1][2,3]"
+    assert_equal [
+      [nil, "join", "join", "1", "2"],
+      [nil, "join", "join", "1", "2,3"]
+    ], parser.specs
+  end
+  
+  def test_join_does_not_infer_lead_or_end_index
+    parser.parse "--[][] --[1][] --[][2]"
+    assert_equal [
+      [nil, "join", "join", "", ""],
+      [nil, "join", "join", "1", ""],
+      [nil, "join", "join", "", "2"]
+    ], parser.specs
+  end
+  
+  def test_join_with_modifier
+    parser.parse  "--[][]is.class"
+    assert_equal [
+      [nil, "join", "class", "", "", "-i", "-s"]
+    ], parser.specs
+  end
+  
+  #
+  # signal tests
+  #
+  
+  def test_parser_parses_signals
+    parser.parse  "--/variable signal a b c"
+    assert_equal [
+      ["variable", nil, "signal", "a", "b", "c"]
+    ], parser.specs
+  end
+  
+  #
+  # spec tests
+  #
+  
+  def test_parser_parses_spec
+    parser.parse  "--. type class a b c"
+    assert_equal [
+      ["0", "type", "class", "a", "b", "c"]
+    ], parser.specs
+  end
+  
+  #
+  # job tests
+  #
+  
+  def test_parser_parses_job
+    parser.parse  "--@ a b c"
+    assert_equal [
+      [nil, nil, "enque", "a", "b", "c"]
+    ], parser.specs
+  end
+end
