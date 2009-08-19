e5057eaa0634a1f6364ce9d7e93236bd4f1bec2e
Simon Chiang <simon.a.chiang@gmail.com>
2009-08-19 14:53:53 -0600

    added app.env initialization to TapTest

--- tap-test/lib/tap/test/tap_test.rb
+++ tap-test/lib/tap/test/tap_test.rb
@@ -3,19 +3,27 @@ require 'tap/app'
 module Tap
   module Test
     
-    # Simply sets up and tears down Tap::App.instance so that tests that
-    # instantiate classes will not inadvertently smush over into one another.
+    # Sets up and tears down Tap::App.instance and Tap::Env.instance.  This
+    # prevents tests that modify either from inadvertently conflicting with
+    # one another.
     module TapTest
       
       # The test specific app
       attr_reader :app
+      
+      # The test specific env
+      attr_reader :env
 
       def setup
         super
-        @app = Tap::App.instance = Tap::App.new(:debug => true, :quiet => true)
+        @env = Tap::Env.instance = Tap::Env.new(:gems => :none)
+        @app = Tap::App.instance = Tap::App.new(:debug => true, :quiet => true, :env => @env)
+        @env.activate
       end
 
       def teardown
+        @env.deactivate
+        Tap::Env.instance = nil
         Tap::App.instance = nil
         super
       end
