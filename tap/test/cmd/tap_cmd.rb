dd115042a2f4cdd3ab0348366d86a799e49adbf4
Simon Chiang <simon.a.chiang@gmail.com>
2009-09-04 10:12:11 -0600

    added tests for specifying an env key on a command

--- tap/test/cmd/tap_cmd.rb
+++ tap/test/cmd/tap_cmd.rb
@@ -17,6 +17,10 @@ class CmdDoc < Test::Unit::TestCase
     super
   end
   
+  #
+  # help test
+  #
+  
   def test_tap_prints_help
     sh_match "% tap", 
     /usage: tap/, 
@@ -27,11 +31,44 @@ class CmdDoc < Test::Unit::TestCase
     /version #{Tap::VERSION}/
   end
   
-  def test_tap_handles_unknown_command
+  #
+  # command test
+  #
+  
+  def test_tap_notifies_unknown_command
     sh_test %Q{
 % tap unknown
 Unknown command: 'unknown'
 Type 'tap --help' for usage information.
+}
+  end
+  
+  def test_tap_loads_command
+    method_root.prepare(:cmd, 'cmd.rb') do |io|
+      io << "puts 'goodnight moon'"
+    end
+    
+    sh_test %Q{
+% tap cmd
+goodnight moon
+}
+  end
+  
+  def test_commands_may_specify_env
+    method_root.prepare(:cmd, 'run.rb') do |io|
+      io << "puts 'alternative run'"
+    end
+    
+    assert File.basename(Dir.pwd) != 'tap'
+    
+    sh_test %Q{
+% tap tap:run -- dump 'standard run'
+standard run
+}
+
+    sh_test %Q{
+% tap #{File.basename(Dir.pwd)}:run -- dump 'standard run'
+alternative run
 }
   end
 end
