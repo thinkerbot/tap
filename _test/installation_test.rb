083bcace633617899436e9c95c6c73e7be1182aa
Simon Chiang <simon.a.chiang@gmail.com>
2009-12-19 10:53:44 -0700

    updated installation test

--- _test/installation_test.rb
+++ _test/installation_test.rb
@@ -2,10 +2,10 @@ require File.dirname(__FILE__) + "/test_helper"
 
 class InstallationTest < Test::Unit::TestCase
   acts_as_shell_test
-  acts_as_file_test :cleanup_dirs => [:sample]
+  acts_as_file_test
   
   def test_online_docs
-    method_root.prepare(:sample, "lib/goodnight.rb") do |io|
+    method_root.prepare("lib/goodnight.rb") do |io|
       io << %q{
       # Goodnight::task your basic goodnight moon task
       # Says goodnight with a configurable message.
@@ -17,27 +17,45 @@ class InstallationTest < Test::Unit::TestCase
         end
       end}
     end
-    Dir.chdir(method_root.path(:sample))
+    Dir.chdir(method_root.root)
     
     with_env('TAP_GEMS' => '') do
       sh_test %q{
-% tap run -- goodnight moon --: dump
-goodnight moon
-}
+      % tap run -- goodnight moon --: dump
+      goodnight moon
+      }
     
       sh_test %q{
-% tap run -- load mittens -- load boat -- goodnight -- dump --[0,1][2] --[2][3]
-goodnight mittens
-goodnight boat
-}
+      % tap run -- load mittens -- load boat -- goodnight -- dump --[0,1][2] --[2][3]
+      goodnight mittens
+      goodnight boat
+      }
+      
+      sh_test %Q{
+      % tap run -T
+      #{method_name}:
+        goodnight   # your basic goodnight moon task
+      tap:
+        dump        # the default dump task
+        load        # the default load task
+      }
+      
       sh_test %q{
-% tap run -T
-sample:
-  goodnight   # your basic goodnight moon task
-tap:
-  dump        # the default dump task
-  load        # the default load task
-}
+      % tap run -- goodnight --help
+      Goodnight -- your basic goodnight moon task
+      --------------------------------------------------------------------------------
+        Says goodnight with a configurable message.
+      --------------------------------------------------------------------------------
+      usage: tap run -- goodnight NAME
+      
+      configurations:
+              --message MESSAGE            a goodnight message
+      
+      options:
+              --help                       Print this help
+              --enque                      Manually enques self
+              --config FILE                Specifies a config file
+      }
     end
   end
 end
