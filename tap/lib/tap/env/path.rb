c02068a5bc955a4018973f2edf41da9a7410a07f
Simon Chiang <simon.a.chiang@gmail.com>
2010-01-29 15:04:12 -0700

    updated Path with testing

--- tap/lib/tap/env/path.rb
+++ tap/lib/tap/env/path.rb
@@ -1,48 +1,48 @@
-autoload(:YAML, 'yaml')
-
 module Tap
   class Env
     class Path
       class << self
+        
+        # Splits the path string along ':' boundaries and expands each
+        # resulting fragments relative to dir.  Duplicate paths are removed. 
+        # Returns the resulting paths.
+        #
+        # An array of pre-split paths may also be provided as an input.
         def split(str, dir=Dir.pwd)
           paths = str.kind_of?(String) ? str.split(':') : str
           paths.collect! {|path| File.expand_path(path, dir) }
           paths.uniq!
           paths
         end
-        
-        def load(path)
-          path_file = File.join(path, FILE)
-          map = Root.trivial?(path_file) ? {} : (YAML.load_file(path_file) || {})
-          new(path, map)
-        end
-        
-        def loadable?(path)
-          File.exists? File.join(path, FILE)
-        end
       end
       
-      FILE = 'tap.yml'
-      
+      # The path base.
       attr_reader :base
+      
+      # A mapping of types to paths.
       attr_reader :map
       
+      # Creates a new Path relative to the base.
       def initialize(base, map={})
         @base = File.expand_path(base)
-        @map = Hash.new do |hash, type|
-          hash[type] = [File.expand_path(type.to_s, @base)]
-        end
+        @map = {}
         
-        map.each_pair do |type, paths|
-          paths = [paths] unless paths.kind_of?(Array)
-          @map[type] = paths.collect {|path| File.expand_path(path, @base) }
-        end
+        map.each_pair {|type, paths| self[type] = paths }
       end
       
+      # Returns an array of expanded paths associated with the type; by
+      # default the type expanded under base.
       def [](type)
-        map[type]
+        map[type] ||= [File.expand_path(type.to_s, base)]
       end
       
+      # Sets the path for the type.  Paths are split and expanded relative to
+      # base (see Path.split).
+      def []=(type, paths)
+        map[type] = Path.split(paths, base)
+      end
+      
+      # Returns the base path.
       def to_s
         base
       end
---
+++ tap/test/tap/env/path_test.rb
@@ -0,0 +1,56 @@
+require File.join(File.dirname(__FILE__), '../../tap_test_helper')
+require 'tap/env/path'
+require 'tap/test'
+
+class PathTest < Test::Unit::TestCase
+  extend Tap::Test
+  acts_as_file_test
+  
+  Path = Tap::Env::Path
+  attr_reader :path
+  
+  def setup
+    super
+    @path = Path.new(method_root.path)
+  end
+  
+  #
+  # initialize test
+  #
+  
+  def test_initialize_splits_and_expands_map
+    path = Path.new(method_root.path, :array => ['/a/b', 'c'], :string => '/e/f:g')
+    assert_equal({
+      :array => ['/a/b', method_root.path('c')],
+      :string => ['/e/f', method_root.path('g')]
+    }, path.map)
+  end
+  
+  #
+  # AGET test
+  #
+  
+  def test_AGET_returns_type_joined_to_base_for_unspecified_mappings
+    assert path.map.empty?
+    assert_equal [method_root.path(:type)], path[:type]
+  end
+  
+  def test_AGET_returns_mapped_paths_for_named_type
+    path.map[:type] = ['/a/b']
+    assert_equal ['/a/b'], path[:type]
+  end
+  
+  #
+  # ASET test
+  #
+  
+  def test_ASET_sets_path_for_type_expanding_relative_to_base_as_necessary
+    path[:type] = ['/a/b', 'c/d']
+    assert_equal({:type => ['/a/b', method_root.path('c/d')]}, path.map)
+  end
+  
+  def test_ASET_splits_and_arrayifies_string_inputs
+    path[:type] = '/a/b:c/d'
+    assert_equal({:type => ['/a/b', method_root.path('c/d')]}, path.map)
+  end
+end
