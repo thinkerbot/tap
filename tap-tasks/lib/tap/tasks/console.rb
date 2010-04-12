a655a9003e8be09dce21e028972b4d2b3dacc67c
Simon Chiang <simon.a.chiang@gmail.com>
2010-04-11 22:53:13 -0600

    added a console task

---
+++ tap-tasks/lib/tap/tasks/console.rb
@@ -0,0 +1,45 @@
+require 'tap/task'
+require 'tap/declarations'
+require 'irb'
+
+# http://www.ruby-forum.com/topic/182335
+class IRB::Irb
+  alias initialize_orig initialize
+  def initialize(workspace = nil, *args)
+    default = IRB.conf[:DEFAULT_OBJECT]
+    workspace ||= IRB::WorkSpace.new(default) if default
+    initialize_orig(workspace, *args)
+  end
+end
+
+module Tap
+  module Tasks
+    # :startdoc::task start an irb session
+    class Console < Tap::Task
+      include Tap::Declarations
+      
+      # Handles a bug in IRB that causes exit to throw :IRB_EXIT
+      # and consequentially make a warning message, even on a
+      # clean exit. This module resets exit to the original
+      # aliased method.
+      module CleanExit # :nodoc:
+        def exit(ret = 0)
+          __exit__(ret)
+        end
+      end
+      
+      def initialize(*args)
+        super
+        initialize_declare
+      end
+      
+      def process
+        raise "console already running" if IRB.conf[:DEFAULT_OBJECT]
+        IRB.conf[:DEFAULT_OBJECT] = self
+        IRB.start
+        IRB.conf[:DEFAULT_OBJECT] = nil
+        IRB.CurrentContext.extend CleanExit
+      end
+    end 
+  end
+end
