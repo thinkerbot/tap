5b8fbc1136a08c55a9e599284e5c58e7a80edb81
Simon Chiang <simon.a.chiang@gmail.com>
2010-04-18 21:51:13 -0600

    added logging to App#exe and updated debugger to not print duplicate information

--- tap/lib/tap/middlewares/debugger.rb
+++ tap/lib/tap/middlewares/debugger.rb
@@ -4,13 +4,27 @@ module Tap
   module Middlewares
     
     # :startdoc::middleware default debugger
+    #
+    # Logs the execution of tasks with their inputs.  Debugger outputs the
+    # same information as App.exe will output when the app is set to debug. To
+    # avoid duplication, debugger ONLY logs execution when the app is not in
+    # debug mode, or when force is set.
+    #
     class Debugger < Middleware
+      config :force, false, &c.flag    # Force logging
+      
       def call(task, input)
-        app.log "#{app.var(task)}", "<< #{summarize input} (#{task.class})"
+        log("#{app.var(task)} <<", "#{summarize input} (#{task.class})")
         output = super
         
-        app.log "#{app.var(task)}", ">> #{summarize output} (#{task.class})"
+        log("#{app.var(task)} >>", "#{summarize output} (#{task.class})")
         output
+      end
+      
+      def log(action, msg)
+        if force || !app.debug
+          app.log(action, msg)
+        end
       end
       
       def summarize(obj)
