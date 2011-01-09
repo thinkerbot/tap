a072227593ff353637e95e979f1ea528a3d8f1ec
Simon Chiang <simon.a.chiang@gmail.com>
2011-01-09 07:41:14 -0700

    version tap-test-0.7.0

--- tap-test/lib/tap/test/version.rb
+++ tap-test/lib/tap/test/version.rb
@@ -1,9 +1,9 @@
 module Tap
   module Test
     MAJOR = 0
-    MINOR = 6
-    TINY = 0
-
-    VERSION="#{MAJOR}.#{MINOR}.#{TINY}"
+    MINOR = 7
+    TINY  = 0
+    
+    VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
   end
 end
