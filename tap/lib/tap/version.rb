80621c6799849791a8e24203046d6b1f8a9dec60
Simon Chiang <simon.a.chiang@gmail.com>
2011-01-09 07:39:28 -0700

    version tap-1.4.0

--- tap/lib/tap/version.rb
+++ tap/lib/tap/version.rb
@@ -1,8 +1,8 @@
 module Tap
   MAJOR = 1
-  MINOR = 3
-  TINY = 0
+  MINOR = 4
+  TINY  = 0
   
-  VERSION="#{MAJOR}.#{MINOR}.#{TINY}" 
-  WEBSITE="http://tap.rubyforge.org"
+  VERSION = "#{MAJOR}.#{MINOR}.#{TINY}" 
+  WEBSITE = "http://tap.rubyforge.org"
 end
