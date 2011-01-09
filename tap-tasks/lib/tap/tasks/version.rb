87d676f9d6c0288cbbad1c798f141f8940784e7c
Simon Chiang <simon.a.chiang@gmail.com>
2011-01-09 07:44:22 -0700

    version tap-tasks-0.8.0

--- tap-tasks/lib/tap/tasks/version.rb
+++ tap-tasks/lib/tap/tasks/version.rb
@@ -1,9 +1,9 @@
 module Tap
   module Tasks
     MAJOR = 0
-    MINOR = 7
-    TINY = 0
-
-    VERSION="#{MAJOR}.#{MINOR}.#{TINY}"
+    MINOR = 8
+    TINY  = 0
+    
+    VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
   end
 end
