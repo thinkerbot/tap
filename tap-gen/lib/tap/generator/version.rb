61016bace6d608d567782bb945d3bd4afe024474
Simon Chiang <simon.a.chiang@gmail.com>
2011-01-09 07:42:17 -0700

    version tap-gen-0.8.0

--- tap-gen/lib/tap/generator/version.rb
+++ tap-gen/lib/tap/generator/version.rb
@@ -1,9 +1,9 @@
 module Tap
   module Generator
     MAJOR = 0
-    MINOR = 7
-    TINY = 1
+    MINOR = 8
+    TINY  = 0
     
-    VERSION="#{MAJOR}.#{MINOR}.#{TINY}"
+    VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
   end
 end
